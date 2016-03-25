c04aba71af04b8832401041b82f8baa1a62df17a
Paul <pacalabre@gmail.com>
2016-03-24 19:36:58 -0700

    added comments to JS

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,31 +1,32 @@
 
-function fToC(fahrenheit)
-{
+// Temp converter
+function fToC(fahrenheit) {
   var fTemp = fahrenheit;
   var fToCel = (fTemp - 32) * 5 / 9;
   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
   return fToCel;
 }
 
+// Listen for the clicked button
 document.getElementById('convert').addEventListener('click',function() {
   console.log('clicked');
 });
 
-
+// Once the form is submitted, run the conversion with the input value,
+//and then display it on the page
 document.getElementById('form').addEventListener('submit',function(event) {
   event.preventDefault();
   var inputReceived = document.getElementById('input').value;
   var valConversion = fToC(inputReceived);
-  //console.log('input received= '+ inputReceived);
-  //console.log('val conversion received= '+ valConversion);
   document.getElementById('displayNewTemp').innerHTML=valConversion;
 });
 
+// When the clear button is pressed, clear out the input field
+// and remove the temperature from the page
 document.getElementById('clear').addEventListener('click',function() {
   event.preventDefault();
   document.getElementById('displayNewTemp').innerHTML="";
   document.getElementById('input').value ="";
-
 });
 
 
