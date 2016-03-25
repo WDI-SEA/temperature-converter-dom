757d1308579601eb738ad7fe70c84ff261dba82a
brytree <brycebortree@gmail.com>
2016-03-24 19:47:38 -0700

    now without script html tags

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -3,5 +3,5 @@ function fToC(fahrenheit)
   var fTemp = fahrenheit;  
   var fToCel = (fTemp - 32) * 5 / 9;  
   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';  
-    console.log(message);  
+    alert(message);  
 }   
