30efe9dffe6ba1dc471cf19a968d645e9024af76
keidra <keidrabaldwin@gmail.com>
2016-03-25 10:29:56 -0700

    added background color for temps

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -6,21 +6,19 @@ document.getElementById('button1').addEventListener('click', function() {
         var cTempVal = (fTempVal - 32) * (5 / 9);
         console.log(cTempVal);
         document.getElementById('output').textContent = Math.round(cTempVal);
-
     }
 
 
     convertToC();
 
     //Attempt to have the div turn blue for freezing temperatures, red for scorching temperatures.
+    if (document.getElementById('output').innerHTML < 0) {
+        document.getElementById('output').style.backgroundColor ="blue";
+        } else {
+        document.getElementById('output').style.backgroundColor ="red";
 
-//     if (convertToC > 38) {
-//         document.getElementById('').style.backgroundColor = "red";{
-//             else if ( convertToC < 40){
-//                 document.getElementById("result").style.backgroundColor = "blue";;
-//             }
-//         }
-//     }
+   }
+
 
 });
 
