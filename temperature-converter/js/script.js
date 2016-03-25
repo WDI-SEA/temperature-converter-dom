9760cf602a0238108f11027538495d3f96e33dba
brytree <brycebortree@gmail.com>
2016-03-24 20:29:08 -0700

    working on getting the display displaying

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -3,7 +3,7 @@
 function fToC()   {  
   var fTemp = document.getElementById('fahrenheit').value;  
   var fToCel = (fTemp - 32) * (5 / 9);  
-  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';  
-    console.log(message);
+  var alertToUser = fTemp+'\xB0F is ' + fToCel + '\xB0C.';  
+  document.getElementById('message').value = alertToUser;
 }   
 
