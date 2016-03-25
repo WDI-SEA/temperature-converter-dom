dd1547283235c845578e4bcf1cc26487825f819c
brytree <brycebortree@gmail.com>
2016-03-24 21:53:20 -0700

    homework push

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -3,7 +3,12 @@
 function fToC()   {  
   var fTemp = document.getElementById('fahrenheit').value;  
   var fToCel = (fTemp - 32) * (5 / 9);  
-  var alertToUser = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+  var alertToUser = fToCel + '\xB0C.';
   document.getElementById("message").innerHTML = alertToUser;
 }   
 
+
+// var x = document.getElementById("audio");
+//   x.autoplay = true;
+//   x.load();
+// More relic code from sarcastic national anthem
