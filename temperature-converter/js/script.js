11c9904aa3143543a4233a615f1a05bafaaeb8b7
brusinow <brusinow@me.com>
2016-03-24 23:58:28 -0700

    tried color change div, commented it out

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,24 +1,39 @@
 
+document.getElementById('clear').addEventListener('click', function(event){
+  document.getElementById('userinput').value = '';
+  document.getElementById('conversion').textContent = '';
+});
 
 document.getElementById('submit').addEventListener('click', function(event){
   console.log(event);
   var f = document.getElementById('userinput').value;
 
+  var fToC = (5/9) * (f-32);
+  var cToF = f * 9/5 + 32;
+
   var tempOutput = function(f) { 
     if (document.getElementById('c-radio').checked === true){
-      return (5/9) * (f-32);
-}   else if (document.getElementById('f-radio').checked === true){
-      return f * 9/5 + 32;
+      return fToC;
 }   else {
-      return '';
+      return cToF;
 }
 }
+
   document.getElementById('conversion').textContent = (tempOutput(f)).toFixed(1) + " Degrees";
 });
 
-document.getElementById('clear').addEventListener('click', function(event){
-  document.getElementById('userinput').value = '';
-  document.getElementById('conversion').textContent = '';
-});
+//     if (document.getElementById('f-radio').checked && (tempOutput(f)) > 90) {
+//       document.getElementById('conversion').style.color = "red";
+// }   else if (document.getElementById('c-radio').checked && (tempOutput(f)) > 32) {
+//       document.getElementById('conversion').style.color = "red";
+// }   else if (document.getElementById('f-radio').checked && (tempOutput(f)) < 32) {
+//       document.getElementById('conversion').style.color = "blue";
+// }   else if (document.getElementById('c-radio').checked && (tempOutput(f)) < 0) {
+//       document.getElementById('conversion').style.color = "blue";
+// }   else {
+//     '';
+// }
+
+
 
 
