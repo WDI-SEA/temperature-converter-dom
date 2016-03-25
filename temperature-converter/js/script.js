cef3ed226c090a3f6f1804bc67b8b6b4159eab66
mattsluis <Sluismatthew@gmail.com>
2016-03-25 09:09:26 -0700

    moretweaks

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,22 +1,22 @@
 function rset() {
-  console.log("reset")
-  document.getElementById("idForm").reset()
-  var elResult  = document.getElementById("idOut")
+  console.log("reset");
+  document.getElementById("idForm").reset();
+  var elResult  = document.getElementById("idOut");
   elResult.innerHTML =""
 }
 function convert() {
-    var unitF = document.getElementById("idF").checked
-  var temp = document.getElementById("temp").value
-  console.log(temp)
-  console.log(unitF)
+    var unitF = document.getElementById("idF").checked ;
+    var temp = document.getElementById("temp").value ;
+    console.log(temp) ;
+    console.log(unitF) ;
     if (unitF ) {
-    var tmp = (temp -32) * 5 / 9;
-    unitO = " C"
+      var tmp = (temp -32) * 5 / 9;
+      unitO = " C" ;
     } else  {
-        var tmp =  (temp * 9 / 5) + 32;
-    unitO = " F"
+      var tmp =  (temp * 9 / 5) + 32;
+      unitO = " F" ;
     }
-]
-  tmp = Math.round(tmp)
-  console.log(tmp + unitO )
-    document.getElementById("idOut").innerHTML = "equals " + tmp + unitO
+    tmp = Math.round(tmp);
+    console.log(tmp + unitO );
+    document.getElementById("idOut").innerHTML = ("equals " + tmp + unitO);
+}
