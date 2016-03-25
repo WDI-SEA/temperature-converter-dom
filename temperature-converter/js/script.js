4ca4bc47ee967ab359cc6217c3f2e004374518c7
Paul <pacalabre@gmail.com>
2016-03-24 18:39:05 -0700

    added conversion code - need to add code to display conversion to page

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -7,6 +7,15 @@ function fToC(fahrenheit)
     console.log(message);
 }
 
-document.getElementById('convert').addEventListener('click',function(){
-  ;
+document.getElementById('convert').addEventListener('click',function() {
+  console.log('clicked');
 });
+
+document.getElementById('form').addEventListener('submit',function(event) {
+  event.preventDefault();
+  var inputReceived = document.getElementById('input').value;
+  var valConversion = fToC(inputReceived);
+  console.log('inputReceived = '+ valConversion);
+});
+
+
