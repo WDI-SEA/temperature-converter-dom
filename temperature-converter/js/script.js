e16a0509680ebfedea7e7d7a9ba617582a0175fa
StrongSad <jacobkirkbarrus@gmail.com>
2016-03-24 22:02:19 -0700

    got both c and f to work on own in seperate functions. Having a lot of touble figuring out innerhtml. I think im going to call it for today

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,4 +1,8 @@
+// var getTemp = document.getElementById('temp').value;
+// var conversion;
+
 //event listeners
+
 
 calculate = function() {
 	var getTemp = document.getElementById('temp').value;
@@ -15,24 +19,48 @@ calculate = function() {
 			console.log('Man this is a nice temp.... unless its still hot....');
 			document.body.style['background-color'] = 'white';
 		}
-	} else {
-		conversion = (getTemp - 32) * (5/9);
-		console.log(conversion)
-		if (conversion <= 0 ) {
-			document.body.style['background-color'] = 'blue';
-		} else if (conversion >= 100) {
-			document.body.style['background-color'] = 'orange';
-		} else {
-			console.log('Man this is a nice temp.... unless its still hot....');
-			document.body.style['background-color'] = 'white';
-		}
 	}
 }
 
-document.getElementById("calc").addEventListener('click', calculate) 
+
+
+farToC = function() {
+	var getTemp = document.getElementById('temp').value;
+	console.log(getTemp);
+
+	if(document.getElementById('far').checked) {
+		var conversion = (getTemp - 32) * (5/9);
+		console.log(conversion)
+		if (conversion <= 0 ) {
+			document.body.style['background-color'] = 'blue';
+			console.log("brrrrrrr it\s could outside...")
+		} else if (conversion >= 100) {
+			document.body.style['background-color'] = 'orange';
+			console.log("why\s it sooo stinking hot!")
+		} else {
+			console.log('Man this is a nice temp.... unless its still hot....');
+			document.body.style['background-color'] = 'white';
+		}	
+	}
+}
+
+document.getElementById("calc").addEventListener('click', calculate);
+document.getElementById("calc").addEventListener('click', farToC);
 
 
 
+ 
+// 	conversion = (getTemp - 32) * (5/9);
+// 	console.log(conversion)
+// 	if (conversion <= 0 ) {
+// 		document.body.style['background-color'] = 'blue';
+// 	} else if (conversion >= 100) {
+// 		document.body.style['background-color'] = 'orange';
+// 	} else {
+// 		console.log('Man this is a nice temp.... unless its still hot....');
+// 		document.body.style['background-color'] = 'white';
+// 	}
+// }
 
 
 
