b441d6f77e4ad379119a3d7ae344a6c3a59eb429
mattsluis <Sluismatthew@gmail.com>
2016-03-25 10:36:00 -0700

    event listener works

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,20 +1,23 @@
 function convert() {
     var unitF = document.getElementById("idF").checked ;
     var temp = document.getElementById("temp").value ;
-    console.log(temp) ;
-    console.log(unitF) ;
     if (unitF ) {
-      var tmp = (temp -32 * 5 / 9);
+      var tmp = (temp -32) * (5 / 9);
       unitO = " C" ;
     } else  {
-      var tmp =  (temp * 9 / 5 + 32);
+      var tmp =  (temp * 9) / 5 + 32;
       unitO = " F" ;
     }
     tmp = Math.round(tmp);
     console.log(tmp + unitO );
     document.getElementById("idOut").innerHTML = ("equals " + tmp + unitO);
 }
+document.getElementById('calc').addEventListener('click', convert);
+document.getElementById('clear').addEventListener('click', rset);
 function rset() {
-  console.log("reset");
   document.getElementById("idForm").reset();
 }
+
+// get rid of on click and add .eventlisteners
+// document.getElementByid("calc").addeventlistener('click', convert);
+// document.getElementByid("clear").addeventlistener('click', rset);
