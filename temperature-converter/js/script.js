66cca0497ed08cc05982d5b6e8c800080ff57976
Paul <pacalabre@gmail.com>
2016-03-24 19:27:46 -0700

    added clear button

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -11,15 +11,19 @@ document.getElementById('convert').addEventListener('click',function() {
   console.log('clicked');
 });
 
+
 document.getElementById('form').addEventListener('submit',function(event) {
   event.preventDefault();
   var inputReceived = document.getElementById('input').value;
-
   var valConversion = fToC(inputReceived);
-  //var Cel = fToC(fTemp);
-  console.log('input received= '+ inputReceived);
-  console.log('val conversion received= '+ valConversion);
+  //console.log('input received= '+ inputReceived);
+  //console.log('val conversion received= '+ valConversion);
   document.getElementById('displayNewTemp').innerHTML=valConversion;
+});
+
+document.getElementById('clear').addEventListener('click',function() {
+  event.preventDefault();
+  document.getElementById('displayNewTemp').innerHTML="";
 });
 
 
