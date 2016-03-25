15913de1d80cb53b200f3e9ff9f2aef285eb44fd
brusinow <brusinow@me.com>
2016-03-25 09:33:42 -0700

    got div background color change to work

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -2,6 +2,7 @@
 document.getElementById('clear').addEventListener('click', function(event){
   document.getElementById('userinput').value = '';
   document.getElementById('conversion').textContent = '';
+  document.getElementById('conversion').style.backgroundColor = "";
 });
 
 document.getElementById('submit').addEventListener('click', function(event){
@@ -19,20 +20,22 @@ document.getElementById('submit').addEventListener('click', function(event){
 }
 }
 
+   if (document.getElementById('f-radio').checked && (tempOutput(f)) > 212) {
+      document.getElementById('conversion').style.backgroundColor = "red";
+}   else if (document.getElementById('c-radio').checked && (tempOutput(f)) > 100) {
+      document.getElementById('conversion').style.backgroundColor = "red";
+}   else if (document.getElementById('f-radio').checked && (tempOutput(f)) < 32) {
+      document.getElementById('conversion').style.backgroundColor = "blue";
+}   else if (document.getElementById('c-radio').checked && (tempOutput(f)) < 0) {
+      document.getElementById('conversion').style.backgroundColor = "blue";
+}   else {
+    '';
+}
+
   document.getElementById('conversion').textContent = (tempOutput(f)).toFixed(1) + " Degrees";
 });
 
-//     if (document.getElementById('f-radio').checked && (tempOutput(f)) > 90) {
-//       document.getElementById('conversion').style.color = "red";
-// }   else if (document.getElementById('c-radio').checked && (tempOutput(f)) > 32) {
-//       document.getElementById('conversion').style.color = "red";
-// }   else if (document.getElementById('f-radio').checked && (tempOutput(f)) < 32) {
-//       document.getElementById('conversion').style.color = "blue";
-// }   else if (document.getElementById('c-radio').checked && (tempOutput(f)) < 0) {
-//       document.getElementById('conversion').style.color = "blue";
-// }   else {
-//     '';
-// }
+ 
 
 
 
