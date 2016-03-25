200b4a02b9d2046d76a9568266bc493320cac209
montehoover <monte.b.hoover@gmail.com>
2016-03-25 09:07:02 -0700

    Changed to form to submit on enter key.  Added styling.

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,5 +1,5 @@
 var input = document.getElementById('temperature-input');
-var button = document.getElementById('convert-button');
+var form = document.getElementById('converter');
 var result = document.getElementById('result-output');
 
 var whatevernumisininput = 75;
@@ -7,6 +7,7 @@ function conversionFtoC(n) {
     return (n - 32) / 1.8;
 }
 
-button.addEventListener('click', function(element) {
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
     result.innerText = conversionFtoC(input.value).toFixed(1) + " degrees in Celcisus";
 })
