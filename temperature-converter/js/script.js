3bda942522bed8655a8a8b99664de601fb2a3f62
brusinow <brusinow@me.com>
2016-03-24 23:21:29 -0700

    working radio buttons

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -5,8 +5,13 @@ document.getElementById('submit').addEventListener('click', function(event){
   var f = document.getElementById('userinput').value;
 
   var tempOutput = function(f) { 
-    return (5/9) * (f-32);
-    
+    if (document.getElementById('c-radio').checked === true){
+      return (5/9) * (f-32);
+}   else if (document.getElementById('f-radio').checked === true){
+      return f * 9/5 + 32;
+}   else {
+      return '';
+}
 }
   document.getElementById('conversion').textContent = (tempOutput(f)).toFixed(1) + " Degrees";
 });
