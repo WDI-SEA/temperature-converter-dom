b1becceb9e4cda99e1b4491e00cb0493a999b5e7
emilieg <emilie.gerber@hotmail.com>
2016-03-24 18:36:20 -0700

    added function in js file and html

---
+++ temperature-converter/js/script.js
@@ -0,0 +1,45 @@
+// Create a single page using HTML/CSS that contains an <input> field, a <button> and a <div>.
+// Convert the temperature from F to C using the formula we created earlier this week (or Google it).
+// User Experience
+
+// User puts in a value into the text field
+// User clicks on button
+// div gets updated with converted value.
+// Make a clear button to reset the fields
+
+
+
+//need to add event listener here IF YOU HEAR SUBMIT BUTTON CLICK RUN THIS FUNCTION
+//var tempC;
+document.getElementById("submit").addEventListener("click", function(){
+  //empty function
+  function convertToC() {
+ var tempF = document.getElementById("input").value; 
+  console.log(tempF);
+  
+
+    var temperatureC = (tempF - 32) * (5 / 9);
+       console.log(temperatureC); 
+   document.getElementById("output").textContent = temperatureC;
+ }
+ 
+ convertToC();
+});
+
+//textContent => calls output and anything else but value
+
+// .value is for input only
+// .textContent is for anything else
+
+
+//need to add event listener here IF YOU HEAR RESET BUTTON CLICK RUN THIS FUNCTION
+document.getElementById("reset").addEventListener("click", function(){
+  document.getElementById('input').value ="";
+  document.getElementById('output').textContent ="";
+  
+})
+
+// })
+// function myFunction() {
+//   document.getElementById("input").reset();
+// }
