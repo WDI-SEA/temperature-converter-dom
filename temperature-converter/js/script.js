32db2839921da908f5bb791f5c7e59e1f4c4b944
Paul <pacalabre@gmail.com>
2016-03-24 17:44:36 -0700

    added js file and some basic js code

---
+++ temperature-converter/js/script.js
@@ -0,0 +1,12 @@
+
+function fToC(fahrenheit)
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+}
+
+document.getElementById('convert').addEventListener('click',function(){
+  ;
+});
