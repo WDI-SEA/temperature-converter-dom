5747bc6a371c0e6b49924d93e18d11006b145bf5
Hannah Barnes <hlmbarnes@me.com>
2016-03-24 21:36:16 -0700

    updated .js with document.querySelector

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -3,5 +3,14 @@ var convertFToC = function(degrees_f){
 		return (degrees_f)*.5556 - 32;
 	}
 
+var generateCel = function (){
+	if(document.querySelector('Degree-form').length !==0){
+		return; 
+	}
+
+document.getElementById('Degree-form').onsubmit = convertFToC() {myFunction()};
+
 
 }
+
+
