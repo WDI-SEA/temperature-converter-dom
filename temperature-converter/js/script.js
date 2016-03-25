f1f9a755e9df6ddf5bd23d41d1038fba63470477
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-25 02:47:26 -0700

    Completed scripts to display formatted results and reset form

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -6,6 +6,7 @@ var result = document.querySelector('#result');
 var temp = document.querySelector('#temp');
 var toC = document.querySelector('#toC');
 var toF = document.querySelector('#toF');
+var clear = document.querySelector('#clear');
 
 document.querySelector('#submit').addEventListener('click', function() {
 
@@ -16,25 +17,53 @@ if ((typeof(parseInt(document.getElementById('field').value) * 1)) === 'number')
 			newTemp = Math.round((fieldValue - 32) * (5 / 9));
 			temp.innerHTML = newTemp + "&#176;C"
 			result.classList.remove('hidden');
-			console.log(newTemp + "&#176;C");
+			if (newTemp >= 32) {
+				result.classList.add('red');
+				result.classList.remove('blue', 'gray')
+			} else if (newTemp <= 4) {
+				result.classList.add('blue');
+				result.classList.remove('red', 'gray')
+			} else {
+				result.classList.add('gray');
+				result.classList.remove('red', 'blue')
+			}
+			console.log(newTemp + " C");
 		} else {
 			newTemp = Math.round(fieldValue * (9/5) + 32);
-			temp.innerHTML = newTemp + "&#176;F"
+			temp.innerHTML = newTemp + " F"
 			result.classList.remove('hidden');
+			if (newTemp >= 90) {
+				result.classList.add('red');
+				result.classList.remove('blue', 'gray')
+			} else if (newTemp <= 40) {
+				result.classList.add('blue');
+				result.classList.remove('red', 'gray')
+			} else {
+				result.classList.add('gray');
+				result.classList.remove('red', 'blue')
+			}
 			console.log(newTemp + "&#176;F");
 		};
 	} else {alert("Please enter a valid temperature value.");};
 });
 
-document.querySelector('#toF').addEventListener('click', function() {
+toF.addEventListener('click', function() {
 	toC.classList.remove('active');
-	toF.classList.add('active')
+	toF.classList.add('active');
+	temp.innerHTML = "";
+});
+
+toC.addEventListener('click', function() {
+	toF.classList.remove('active');
+	toC.classList.add('active');
 	temp.innerHTML = ""
 });
 
-document.querySelector('#toC').addEventListener('click', function() {
+clear.addEventListener('click', function() {
 	toF.classList.remove('active');
-	toC.classList.add('active')
-	temp.innerHTML = ""
+	toC.classList.add('active');
+	temp.innerHTML = "";
+	result.classList.add('hidden');
+	field.value = "";
 });
 
