2a18c0d52d1bd904074da2c7fa538079dc14d721
brytree <brycebortree@gmail.com>
2016-03-25 10:02:30 -0700

    audio attempts + added an eventListener during review

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,14 +1,27 @@
 
 
-function fToC()   {  
-  var fTemp = document.getElementById('fahrenheit').value;  
-  var fToCel = Math.floor((fTemp - 32) * (5 / 9));  
-  var alertToUser = fToCel + '\xB0C.';
-  document.getElementById("message").innerHTML = alertToUser;
-}   
+// function fToC()   {  
+//   var fTemp = document.getElementById('fahrenheit').value;  
+//   var fToCel = Math.floor((fTemp - 32) * (5 / 9));  
+//   var alertToUser = fToCel + '\xB0C.';
+//   document.getElementById("message").innerHTML = alertToUser;
+// }   
+
+var element = document.getElementById("clickEvent");
+
+element.addEventListener('click', function() { 
+    var fTemp = document.getElementById('fahrenheit').value;  
+    var fToCel = Math.floor((fTemp - 32) * (5 / 9));  
+    var alertToUser = fToCel + '\xB0C.';
+    document.getElementById("message").innerHTML = alertToUser;
+  }, false);
 
 
-// var x = document.getElementById("audio");
-//   x.autoplay = true;
-//   x.load();
+var x = document.getElementsByClass("playAudio");
+
+x.addEventListener('click', function() { 
+  x.play();
+});
+
+
 // More relic code from sarcastic national anthem
