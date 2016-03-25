e84ddda10e73859e6cf68f0fb1e06c6bf62921fb
brytree <brycebortree@gmail.com>
2016-03-24 22:11:06 -0700

    the results are integers

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -2,7 +2,7 @@
 
 function fToC()   {  
   var fTemp = document.getElementById('fahrenheit').value;  
-  var fToCel = (fTemp - 32) * (5 / 9);  
+  var fToCel = Math.floor((fTemp - 32) * (5 / 9));  
   var alertToUser = fToCel + '\xB0C.';
   document.getElementById("message").innerHTML = alertToUser;
 }   
