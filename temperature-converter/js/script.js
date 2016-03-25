b3850fd34f92ff6cf720ad6d50286d3fd0c6b05f
StrongSad <jacobkirkbarrus@gmail.com>
2016-03-25 09:24:12 -0700

    Hitting enter now works for 3 of the 7 functions, didnt have time to work on the other 4. Kelvin is a little harder to get working but c to kel is now up. keyframes frame work is kind of layed, but only in the css. I dont know enough to figure out how to get it to work in js. This stuff is super fun

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,9 +1,10 @@
 
-
+var container = document.getElementById('container');
 
 // This calculates celsius to farenheit
 
-calculate = function() {
+celToFar = function(event) {
+	event.preventDefault();
 	var getTemp = document.getElementById('temp').value;
 	console.log(getTemp);
 	var element = document.getElementById("results");
@@ -14,6 +15,7 @@ calculate = function() {
 		element.innerHTML = "This converts to: " + conversion + " F";
 		if (conversion <= 32 ) {
 			document.body.style['background-color'] = 'blue';
+			// elem.classList.add('blue');
 		} else if (conversion >= 212) {
 			document.body.style['background-color'] = 'orange';
 		} else {
@@ -23,7 +25,8 @@ calculate = function() {
 	}
 }
 
-document.getElementById("calc").addEventListener('click', calculate);
+document.getElementById("calc").addEventListener('click', celToFar);
+document.getElementById("submit").addEventListener('submit', celToFar);
 
 
 
@@ -47,7 +50,7 @@ farToC = function() {
 			
 		} else if (conversion >= 100) {
 			document.body.style['background-color'] = 'orange';
-			console.log("why\s it sooo stinking hot!")
+			console.log("why\'s it sooo stinking hot!")
 		} else {
 			console.log('Man this is a nice temp.... unless its still hot....');
 			document.body.style['background-color'] = 'white';
@@ -56,7 +59,128 @@ farToC = function() {
 }
 
 document.getElementById("calc").addEventListener('click', farToC);
+document.getElementById("submit").addEventListener('submit', farToC);
 
 
+//-------------------------------------------------------------------
+
+
+celkv = function() {
+	var getTemp = parseInt(document.getElementById('temp').value);
+	console.log(getTemp);
+	var element = document.getElementById("results");
+	
+	if(document.getElementById('celkv').checked) {
+		var conversion = Math.floor(273.15 + getTemp);
+		console.log(conversion)
+		element.innerHTML = "This converts to: " + conversion + " Kelvin";
+		if (conversion = 0 ) {
+			document.body.style['background-color'] = 'blue';
+			console.log("This is absolute zero. No cells move, no degeneration, no life. Obtainig absolute zero but more importantly learning to thaw from 0 is key to cryogenic hybernation.")
+			
+		} else if (conversion >= 373.1339) {
+			document.body.style['background-color'] = 'orange';
+			console.log("why\'s it sooo stinking hot!")
+		} else {
+			console.log('Man this is a nice temp.... unless its still hot....');
+			document.body.style['background-color'] = 'white';
+		}	
+	}
+}
+
+document.getElementById("calc").addEventListener('click', celkv);
+document.getElementById("submit").addEventListener('submit', celkv);
+
+
+
+
+
+
+
+farkv = function() {
+	var getTemp = document.getElementById('temp').value;
+	console.log(getTemp);
+	var element = document.getElementById("results");
+	
+	if(document.getElementById('farkv').checked) {
+		var conversion = (getTemp - 32) * (5/9);
+		console.log(conversion)
+		element.innerHTML = "This converts to: " + conversion + " Celsius";
+		if (conversion <= 0 ) {
+			document.body.style['background-color'] = 'blue';
+			console.log("brrrrrrr it\s could outside...")
+			
+		} else if (conversion >= 100) {
+			document.body.style['background-color'] = 'orange';
+			console.log("why\s it sooo stinking hot!")
+		} else {
+			console.log('Man this is a nice temp.... unless its still hot....');
+			document.body.style['background-color'] = 'white';
+		}	
+	}
+}
+
+document.getElementById("calc").addEventListener('click', farkv);
+
+
+
+
+
+
+
+kelvinc = function() {
+	var getTemp = document.getElementById('temp').value;
+	console.log(getTemp);
+	var element = document.getElementById("results");
+	
+	if(document.getElementById('kelvc').checked) {
+		var conversion = (getTemp - 32) * (5/9);
+		console.log(conversion)
+		element.innerHTML = "This converts to: " + conversion + " Celsius";
+		if (conversion <= 0 ) {
+			document.body.style['background-color'] = 'blue';
+			console.log("brrrrrrr it\s could outside...")
+			
+		} else if (conversion >= 100) {
+			document.body.style['background-color'] = 'orange';
+			console.log("why\s it sooo stinking hot!")
+		} else {
+			console.log('Man this is a nice temp.... unless its still hot....');
+			document.body.style['background-color'] = 'white';
+		}	
+	}
+}
+
+document.getElementById("calc").addEventListener('click', kelvinc);
+
+
+
+
+
+
+kelvinf = function() {
+	var getTemp = document.getElementById('temp').value;
+	console.log(getTemp);
+	var element = document.getElementById("results");
+	
+	if(document.getElementById('kelvf').checked) {
+		var conversion = (getTemp - 32) * (5/9);
+		console.log(conversion)
+		element.innerHTML = "This converts to: " + conversion + " Celsius";
+		if (conversion <= 0 ) {
+			document.body.style['background-color'] = 'blue';
+			console.log("brrrrrrr it\s could outside...")
+			
+		} else if (conversion >= 100) {
+			document.body.style['background-color'] = 'orange';
+			console.log("why\s it sooo stinking hot!")
+		} else {
+			console.log('Man this is a nice temp.... unless its still hot....');
+			document.body.style['background-color'] = 'white';
+		}	
+	}
+}
+
+document.getElementById("calc").addEventListener('click', kelvinf);
 
  
