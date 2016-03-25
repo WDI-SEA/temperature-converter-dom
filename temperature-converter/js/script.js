55282f7ae0a2bd214c48d552cb3badb14d56b1a1
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-25 11:39:30 -0700

    Add setResults function, cleaned up code

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -8,8 +8,26 @@ var temp = document.querySelector('#temp');
 var toC = document.querySelector('#toC');
 var toF = document.querySelector('#toF');
 var clear = document.querySelector('#clear');
-var cColors = {'0':'freezing', '10':'cold', '16':'cool', '27':'warm', '32':'hot', '38':'scorching'};
-var fColors = {'32':'freezing', '50':'cold', '60':'cool', '80': 'warm', '90':'hot', '100':'scorching'};
+//var cColors = {'0':'freezing', '10':'cold', '16':'cool', '27':'warm', '32':'hot', '38':'scorching'};
+//var fColors = {'32':'freezing', '50':'cold', '60':'cool', '80': 'warm', '90':'hot', '100':'scorching'};
+var cColors = {'freezing':0, 'cold':10, 'cool':16, 'warm':27, 'hot':32, 'scorching':38};
+var fColors = {'freezing':32, 'cold':50, 'cool':60, 'warm':80, 'hot':90, 'scorching':100};
+
+var setResults = function(temperature, colorArray) {
+	if (newTemp >= colorArray['scorching']) {
+		result.classList.add('scorching');
+	} else if (newTemp >= colorArray['hot']) {
+		result.classList.add('hot');
+	} else if (newTemp >= colorArray['warm']) {
+		result.classList.add('warm');
+	} else if (newTemp <= colorArray['freezing']) {
+		result.classList.add('freezing');
+		} else if (newTemp <= colorArray['cold']) {
+		result.classList.add('cold');
+	} else if (newTemp <= colorArray['cool']) {
+		result.classList.add('cool');
+	} else {result.classList.add('gray');};
+}
 
 document.querySelector('#submit').addEventListener('click', function() {
 
@@ -20,41 +38,12 @@ if ((typeof(parseInt(document.getElementById('field').value) * 1)) === 'number')
 			newTemp = Math.round((fieldValue - 32) * (5 / 9));
 			temp.innerHTML = newTemp + "&#176;C"
 			result.className = "";
-			console.log(result.classList);
-				if (newTemp >= 38) {
-					result.classList.add(cColors['38']);
-				} else if (newTemp >= 32) {
-					result.classList.add(cColors['32']);
-				} else if (newTemp >= 27) {
-					result.classList.add(cColors['27']);
-				} else if (newTemp <= 0) {
-					result.classList.add(cColors['0']);
-					} else if (newTemp <= 10) {
-					result.classList.add(cColors['10']);
-				} else if (newTemp <= 16) {
-					result.classList.add(cColors['16']);
-				} else {result.classList.add('gray');};
-			console.log(newTemp + "&#176;C");
+			setResults(newTemp,cColors);
 		} else {
 			newTemp = Math.round(fieldValue * (9/5) + 32);
 			temp.innerHTML = newTemp + "&#176;F"
-			result.classList.remove('hidden');
 			result.className = "";
-			console.log(result.classList + " " + newTemp);
-			if (newTemp >= 100) {
-					result.classList.add(fColors['100']);
-				} else if (newTemp >= 90) {
-					result.classList.add(fColors['90']);
-				} else if (newTemp >= 80) {
-					result.classList.add(fColors['80']);
-				} else if (newTemp <= 32) {
-					result.classList.add(fColors['32']);
-				} else if (newTemp <= 50) {
-					result.classList.add(fColors['50']);
-				} else if (newTemp <= 60) {
-					result.classList.add(fColors['60']);
-				} else {result.classList.add('gray');};
-			console.log(newTemp + "&#176;F");
+			setResults(newTemp, fColors);
 		};
 	} else {alert("Please enter a valid temperature value.");};
 });
