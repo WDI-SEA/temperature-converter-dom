0775932652d6419458ccd4ee6ef875ac27d63251
bengus101 <bgustafson29@yahoo.com>
2016-03-25 08:57:13 -0700

    test commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var f = fahrenheit;
+var fToC = (f - 32) * 5 / 9;
+
+var celsius = function(convertTemp) {
+	if (degree === f) document.getElementById('f').(value -32) * 5 / 9;
+	console.log(celsius)
+}
+
