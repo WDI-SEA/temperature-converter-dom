d3b448812ca038459f8956c7ad8ebd354a0f9f64
brytree <brycebortree@gmail.com>
2016-03-24 21:07:24 -0700

    button response! innerHTML

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -4,6 +4,6 @@ function fToC()   {
   var fTemp = document.getElementById('fahrenheit').value;  
   var fToCel = (fTemp - 32) * (5 / 9);  
   var alertToUser = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
-  document.write(alertToUser);
+  document.getElementById("message").innerHTML = alertToUser;
 }   
 
