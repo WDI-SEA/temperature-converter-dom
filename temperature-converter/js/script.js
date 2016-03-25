542fde9e7190e60c38be8ae95205ecf4654b774c
mattsluis <Sluismatthew@gmail.com>
2016-03-25 10:07:58 -0700

    more edits

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,22 +1,20 @@
-function rset() {
-  console.log("reset");
-  document.getElementById("idForm").reset();
-  var elResult  = document.getElementById("idOut");
-  elResult.innerHTML =""
-}
 function convert() {
     var unitF = document.getElementById("idF").checked ;
     var temp = document.getElementById("temp").value ;
     console.log(temp) ;
     console.log(unitF) ;
     if (unitF ) {
-      var tmp = (temp -32) * 5 / 9;
+      var tmp = (temp -32 * 5 / 9);
       unitO = " C" ;
     } else  {
-      var tmp =  (temp * 9 / 5) + 32;
+      var tmp =  (temp * 9 / 5 + 32);
       unitO = " F" ;
     }
     tmp = Math.round(tmp);
     console.log(tmp + unitO );
     document.getElementById("idOut").innerHTML = ("equals " + tmp + unitO);
+}
+function rset() {
+  console.log("reset");
+  document.getElementById("idForm").reset();
 }
