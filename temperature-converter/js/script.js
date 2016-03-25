e32c613b257b004a60f0ade19c6726ee60667b34
StrongSad <jacobkirkbarrus@gmail.com>
2016-03-24 22:24:51 -0700

    I got innerhtml to work. It really is very simple. cleaned up a little bit not much. I am going to attemp some css animations now for my background color change

--- temperature-converter/js/script.js
+++ temperature-converter/js/script.js
@@ -1,16 +1,17 @@
-// var getTemp = document.getElementById('temp').value;
-// var conversion;
 
-//event listeners
 
+
+// This calculates celsius to farenheit
 
 calculate = function() {
 	var getTemp = document.getElementById('temp').value;
 	console.log(getTemp);
+	var element = document.getElementById("results");
 	
 	if (document.getElementById('cel').checked) {
 		var conversion = getTemp * (9/5) + 32;
 		console.log(conversion);
+		element.innerHTML = "This converts to: " + conversion + " F";
 		if (conversion <= 32 ) {
 			document.body.style['background-color'] = 'blue';
 		} else if (conversion >= 212) {
@@ -22,18 +23,28 @@ calculate = function() {
 	}
 }
 
+document.getElementById("calc").addEventListener('click', calculate);
 
+
+
+
+
+
+// this converts farenheit to celsius
 
 farToC = function() {
 	var getTemp = document.getElementById('temp').value;
 	console.log(getTemp);
-
+	var element = document.getElementById("results");
+	
 	if(document.getElementById('far').checked) {
 		var conversion = (getTemp - 32) * (5/9);
 		console.log(conversion)
+		element.innerHTML = "This converts to: " + conversion + " Celsius";
 		if (conversion <= 0 ) {
 			document.body.style['background-color'] = 'blue';
 			console.log("brrrrrrr it\s could outside...")
+			
 		} else if (conversion >= 100) {
 			document.body.style['background-color'] = 'orange';
 			console.log("why\s it sooo stinking hot!")
@@ -44,34 +55,8 @@ farToC = function() {
 	}
 }
 
-document.getElementById("calc").addEventListener('click', calculate);
 document.getElementById("calc").addEventListener('click', farToC);
 
 
 
  
-// 	conversion = (getTemp - 32) * (5/9);
-// 	console.log(conversion)
-// 	if (conversion <= 0 ) {
-// 		document.body.style['background-color'] = 'blue';
-// 	} else if (conversion >= 100) {
-// 		document.body.style['background-color'] = 'orange';
-// 	} else {
-// 		console.log('Man this is a nice temp.... unless its still hot....');
-// 		document.body.style['background-color'] = 'white';
-// 	}
-// }
-
-
-
-
-
-
-// function calculateC = {
-// 	getTemp * (9/5) + 32;
-// 	return
-// }
-
-
-
-// f = c * 9/5 + 32
