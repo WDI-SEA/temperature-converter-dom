5c0ca64323eb99520fef98193f68e241d2b903bb
bengus101 <bgustafson29@yahoo.com>
2016-03-28 21:31:16 -0700

    Cleaned up code. Final submit.

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ var celsius = function() {
 
 	var newTemperature = (temperature - 32) * 5 / 9; 
 	document.getElementById('temp').value = newTemperature;
-	// document.getElementById('output').innerText = newTemperature;
-
-	// var result = -32) * 5 / 9;
-	// console.log(celsius)
 }
-
-
 
 document.getElementById('convert').addEventListener('click', function() {
 	celsius()
@@ -22,23 +16,4 @@ document.getElementById('clear').addEventListener('click', function() {
 	document.getElementById('temp').value = '';
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function thirdDown(yardage, playType, player){
-// 	console.log(yardage + " yard " + playType + " to " + player);
-// }
-
-// thirdDown(22, 'pass', 'Jordy Nelson');
 
