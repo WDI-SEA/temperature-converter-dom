b3be1501bd5fac4a26ca14f143bde4bd204b17a4
bengus101 <bgustafson29@yahoo.com>
2016-03-28 17:43:15 -0700

    CSS changes made.

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-var f = fahrenheit;
-var fToC = (f - 32) * 5 / 9;
+var fToC = (temp - 32) * 5 / 9;
 
-var celsius = function(convertTemp) {
-	if (degree === f) document.getElementById('f').(value -32) * 5 / 9;
-	console.log(celsius)
+var celsius = function() {
+	var temperature = document.getElementById('temp').value; 
+	console.log(temperature)
+
+	var newTemperature = temperature * 2
+	document.getElementById('temp').value = newTemperature;
+
+	// var result = -32) * 5 / 9;
+	// console.log(celsius)
 }
+
+
+
+document.getElementById('convert').addEventListener('click', function() {
+	celsius()
+})
+
+document.getElementById('clear').addEventListener('click', function() {
+	console.log("clear pressed");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function thirdDown(yardage, playType, player){
+// 	console.log(yardage + " yard " + playType + " to " + player);
+// }
+
+// thirdDown(22, 'pass', 'Jordy Nelson');
 
