0dc6745141eccf0f06bebb8fc3f008581b957f18
Brian Hague <bhague1281@gmail.com>
2016-05-27 16:52:38 -0700

    working temperature converter

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log('Hello, front end');
+var tempForm = document.getElementById('tempForm');
+var temperature = document.getElementById('temperature');
+var outputDiv = document.getElementById('convertedTemp');
+
+tempForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log(temperature.value);
+  var convertedTemp = (temperature.value -32) * (5/9);
+  console.log(convertedTemp);
+  outputDiv.innerHTML = convertedTemp;
+});
