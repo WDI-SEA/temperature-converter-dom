ddee8ae7330670217d9ff8cbc43eb3e5ac25ae65
Brian Hague <bhague1281@gmail.com>
2016-05-27 17:01:17 -0700

    cleaned up code with domcontentloaded

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
-var tempForm = document.getElementById('tempForm');
-var temperature = document.getElementById('temperature');
-var outputDiv = document.getElementById('convertedTemp');
+document.addEventListener('DOMContentLoaded', function() {
+  var tempForm = document.getElementById('tempForm');
+  var temperature = document.getElementById('temperature');
+  var outputDiv = document.getElementById('outputDiv');
 
-tempForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  // console.log(temperature.value);
-  var convertedTemp = (temperature.value - 32) * (5 / 9);
-  // console.log(convertedTemp);
-  outputDiv.innerHTML = convertedTemp;
+  tempForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // console.log(temperature.value);
+    var convertedTemp = (temperature.value - 32) * (5 / 9);
+    // console.log(convertedTemp);
+    outputDiv.innerHTML = convertedTemp;
+  });
 });
+
