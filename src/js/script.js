43229b37145737b33f03827d1bd7c6b447d0a3e8
Travis Smith <smithtm90@gmail.com>
2016-05-30 15:08:54 -0700

    temp converter deliverable

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', function() {
+  var tempForm = document.getElementById('tempForm');
+  var temperature = document.getElementById('temperature');
+  var outputDiv = document.getElementById('outputDiv');
+
+  tempForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var convertedTemp = (temperature.value - 32) * (5 / 9);
+    outputDiv.innerHTML = convertedTemp + " Degrees Celcius";
+  });
+});
