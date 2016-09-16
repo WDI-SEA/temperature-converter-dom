77dd4471c714b04249bcec2a59c1c7e507b2cba4
jamiebb <jamieandburton@gmail.com>
2016-09-16 16:07:04 -0700

    Deliverable Completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', function() {
+		
+	var insertedNumber = document.getElementById('insertNumber');
+
+	var submit = document.getElementById('submitted');
+
+	var finalTemp = document.getElementById('finalTemp');
+
+	var deleted = document.getElementById('deleted');
+
+	deleted.addEventListener('click', function() {
+		finalTemp.style.backgroundColor = "";
+		finalTemp.textContent = "";
+		insertedNumber.value = "";
+				
+	});
+
+	submit.addEventListener('click', function() {
+		var result = 0;
+
+	if (isNaN(insertedNumber.value)) {
+		alert("This is not a number!");
+	}
+
+		else if (document.getElementById('celTofah').checked) {
+			result = (insertedNumber.value - 32) * (5/9);
+			finalTemp.textContent = result.toFixed(2) + " Celsius.";
+				if (result > 70) {
+					finalTemp.style.backgroundColor = "red";
+				} else if (result < 21) {
+					finalTemp.style.backgroundColor = "blue";
+				} else {
+					finalTemp.style.backgroundColor = "";
+				}
+		} else if (document.getElementById('fahTocel').checked) {
+			result = (insertedNumber.value) * (9/5) + 32;
+			finalTemp.textContent = result.toFixed(2) + " Fahrenheit.";
+				if (result > 70) {
+					finalTemp.style.backgroundColor = "red";
+				} else if (result < 32) {
+					finalTemp.style.backgroundColor = "blue";
+				} else {
+					finalTemp.style.backgroundColor = "";
+				}
+		} else {
+			alert("Select a converter.");
+		}
+
+	});
+
+});
