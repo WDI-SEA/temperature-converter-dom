8b44ff21ad926109652b9ac8c515f5b8bfceb974
Travis Walentin <travis.walentin@gmail.com>
2016-11-29 16:28:37 -0800

    inital commit of changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log('Hello, front end');
+var input = document.getElementById('form');
+var radioF = input.tempInF;
+var radioC = input.tempInC;
+radioF.addEventListener('click', radioSelector(radioF));
+radioC.addEventListener('click', radioSelector(radioC));
+function radioSelector(option) {
+  if (option.checked === false) {
+    option.checked = true;
+  }
+  return option;
+
+}
+
+
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function convertTemp() {
+    var currentValue = input.tempToConvert.value;
+    if (radioF.checked === true) {
+        // input.tempInF.checked = false;
+        currentValue = (currentValue - 32)/1.8;
+    } else {
+        // input.tempInC.checked = false;
+        currentValue = (currentValue * 1.8) + 32;
+    }
+    return round(currentValue,1);
+}
+
+function drawOutput() {
+
+    document.getElementById("output").innerHTML = convertTemp();
+}
