b66c922b251becf4c585fcb633e25fb18f3895bc
Travis Walentin <travis.walentin@gmail.com>
2016-11-29 22:25:47 -0800

    added enter key functionallity

--- js/script.js
+++ js/script.js
@@ -13,39 +13,39 @@ function radioSelector(option) {
 
 
 function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
 function convertTemp() {
-    var currentValue = input.tempToConvert.value;
-    if (radioF.checked === true) {
-        // input.tempInF.checked = false;
-        currentValue = (currentValue - 32)/1.8;
-    } else {
-        // input.tempInC.checked = false;
-        currentValue = (currentValue * 1.8) + 32;
-    }
-    return round(currentValue,1);
+  var currentValue = input.tempToConvert.value;
+  if (radioF.checked === true) {
+    // input.tempInF.checked = false;
+    currentValue = (currentValue - 32) / 1.8;
+  } else {
+    // input.tempInC.checked = false;
+    currentValue = (currentValue * 1.8) + 32;
+  }
+  return round(currentValue, 1);
 }
 
 function drawOutput() {
-        if (radioC.checked === true) {
-            if (convertTemp() > 89) {
-                document.getElementById('colorBox').style.backgroundColor = "red";
-            } else if (convertTemp() < 50) {
-                document.getElementById('colorBox').style.backgroundColor = "blue";
-            } else {
-                document.getElementById('colorBox').style.backgroundColor = "goldenrod";
-            }
-            document.getElementById("output").innerHTML = convertTemp() + "˚F";
-        } else {
-            if (convertTemp() > 31) {
-                document.getElementById('colorBox').style.backgroundColor = "red";
-            } else if (convertTemp() < 10) {
-                document.getElementById('colorBox').style.backgroundColor = "blue";
-            } else {
-                document.getElementById('colorBox').style.backgroundColor = "goldenrod";
-            }
-            document.getElementById("output").innerHTML = convertTemp() + "˚C";
-        }
+  if (radioC.checked === true) {
+    if (convertTemp() > 89) {
+      document.getElementById('colorBox').style.backgroundColor = "red";
+    } else if (convertTemp() < 50) {
+      document.getElementById('colorBox').style.backgroundColor = "blue";
+    } else {
+      document.getElementById('colorBox').style.backgroundColor = "goldenrod";
     }
+    document.getElementById("output").innerHTML = convertTemp() + "˚F";
+  } else {
+    if (convertTemp() > 31) {
+      document.getElementById('colorBox').style.backgroundColor = "red";
+    } else if (convertTemp() < 10) {
+      document.getElementById('colorBox').style.backgroundColor = "blue";
+    } else {
+      document.getElementById('colorBox').style.backgroundColor = "goldenrod";
+    }
+    document.getElementById("output").innerHTML = convertTemp() + "˚C";
+  }
+}
