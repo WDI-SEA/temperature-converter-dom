08a4d03da8112e5a3aaf56172e6748caf2375ebd
Donnie Ellstrom <dsvene@gmail.com>
2016-11-30 18:43:34 -0800

    Code app logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, front end');
+var userText;
+var userTemp;
+var convertedTemp;
+$(document).ready(function(){
+
+    $("#userInput").keyup(function(){
+        userTemp = parseInt($(this).val());
+    });
+
+    $('.convert').click(function() {
+        if ($('#radioF').is(':checked')) {
+            convertedTemp = (Math.round(((userTemp * (9/5)) + 32)) + "&deg;F");
+            console.log(convertedTemp);
+            $('.result').html(userTemp + "&deg;C converts to " + convertedTemp);
+         }
+         else {
+             convertedTemp = (Math.round(((userTemp - 32) * (5/9))) + "&deg;C");
+             console.log(convertedTemp);
+             $('.result').html(userTemp + "&deg;F converts to " + convertedTemp);
+         }
+    });
+
+        $('.clear').click(function(){
+            $(this).closest('form').find("input[type=text], textarea").val("");
+            $('.result').html("");
+        });
+});
