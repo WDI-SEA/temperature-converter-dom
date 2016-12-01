661bf0f104d40bf760ecdab27d119b2901b21c62
Donnie Ellstrom <dsvene@gmail.com>
2016-12-01 09:08:50 -0800

    Add html to force user input to int, DRY code

--- js/script.js
+++ js/script.js
@@ -12,30 +12,32 @@ $(document).ready(function() {
     //Modify unit type based on radio button status
     $('input[type=radio][name=radioButton]').change(function () {
         if (this.value == "fahrenheit") {
-            document.querySelector(".unitId").innerHTML = "&deg; Celsius";
+            document.querySelector('.unitId').innerHTML = "&deg; Celsius";
         }
         else {
-            document.querySelector(".unitId").innerHTML = "&deg; Fahrenheit";
+            document.querySelector('.unitId').innerHTML = "&deg; Fahrenheit";
         }
     });
 
     //Convert user input to alternate unit type and return result
-    $('.convert').click(function() {
-        if ($('#radioF').is(':checked')) {
-            convertedTemp = (Math.round(((userTemp * (9/5)) + 32)) + "&deg;F");
-            console.log(convertedTemp);
-            $('.result').html(userTemp + "&deg;C converts to " + convertedTemp);
-         }
-         else {
-             convertedTemp = (Math.round(((userTemp - 32) * (5/9))) + "&deg;C");
-             console.log(convertedTemp);
-             $('.result').html(userTemp + "&deg;F converts to " + convertedTemp);
-         }
-    });
+    document.querySelector('.convert').addEventListener('click', function() {
+            if ($('#radioF').is(':checked')) {
+                convertedTemp = (Math.round(((userTemp * (9/5)) + 32)) + "&deg;F");
+                console.log(convertedTemp);
+                document.querySelector('.result').innerHTML = (userTemp + "&deg;C converts to " + convertedTemp);
+            }
+            else {
+                convertedTemp = (Math.round(((userTemp - 32) * (5/9))) + "&deg;C");
+                console.log(convertedTemp);
+                document.querySelector('.result').innerHTML =(userTemp + "&deg;F converts to " + convertedTemp);
+            }
+        });
 
+    //Clears user input and result
     $('.clear').click(function(){
         $(this).closest('form').find("input[type=text], textarea").val("");
         $('.result').html("");
+        userTemp = NaN;
     });
 });
 };
