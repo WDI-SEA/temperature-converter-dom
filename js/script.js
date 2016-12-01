109fe894dabe02b3848d5aa7fa027d88bd7cf039
Ryne Schillinger <ryneschillinger@gmail.com>
2016-12-01 01:01:35 -0800

    completed temperature converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello, front end');
+document.addEventListener("DOMContentLoaded", function() {
+
+	function convertFtoC() {
+		var celsius = document.getElementById("result");
+		var input = document.getElementsByTagName("input")[0];
+		var result = (input.value - 32) * 5/9;
+		celsius.textContent = Math.round(result);
+	}
+
+	document.getElementById("submit").addEventListener("click", function() {
+		convertFtoC();
+	});
+
+	document.getElementById("reset").addEventListener("click", function() {
+		document.getElementById("value").value = "";
+		document.getElementById("result").textContent = "0";
+	});
+
+});
