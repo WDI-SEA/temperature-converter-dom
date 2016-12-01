95595b1efa6419dca8ea9df64b9a88bc2a0e0c2b
Tommy-Lim <tlim24@gmail.com>
2016-12-01 11:33:57 -0800

    temp converter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var submitButton = document.getElementById('submit');
 var clearButton = document.getElementById('clear');
+var degreesNumber = document.getElementById('degrees-number');
+var form = document.getElementById('form');
 
 
 var calcTemp = function(){
@@ -10,11 +12,40 @@ var calcTemp = function(){
     document.getElementById('result').innerHTML = "Invalid Input";
   } else{
     if(unit){
-      document.getElementById('result').innerHTML = ((num-32)/(9/5)).toFixed(1)+" &deg;C";
+      newTemp = (num-32)/(9/5);
+      if(newTemp<0){
+        document.getElementById('result').innerHTML = newTemp.toFixed(1)+" &deg;C";
+        document.getElementById('result').className = "cold";
+      } else if(newTemp>100){
+        document.getElementById('result').innerHTML = newTemp.toFixed(1)+" &deg;C";
+        document.getElementById('result').className = "hot";
+      } else{
+        document.getElementById('result').innerHTML = newTemp.toFixed(1)+" &deg;C";
+        document.getElementById('result').className = "normal";
+      }
+      // document.getElementById('result').innerHTML = ((num-32)/(9/5)).toFixed(1)+" &deg;C";
     } else{
-      document.getElementById('result').innerHTML = ((num)*(9/5)+32).toFixed(1)+" &deg;F";
+      newTemp = (num)*(9/5)+32;
+      if(newTemp<32){
+        document.getElementById('result').innerHTML = newTemp.toFixed(1)+" &deg;F";
+        document.getElementById('result').className = "cold";
+      } else if(newTemp<212){
+        document.getElementById('result').innerHTML = newTemp.toFixed(1)+" &deg;F";
+        document.getElementById('result').className = "hot";
+      } else{
+        document.getElementById('result').innerHTML = newTemp.toFixed(1)+" &deg;F";
+        document.getElementById('result').className = "normal";
+      }
+      // document.getElementById('result').innerHTML = ((num)*(9/5)+32).toFixed(1)+" &deg;F";
     }
   }
+};
+
+var isEnter = function(e){
+	var key = e.which||e.keyCode;
+	if(key===13){
+		calcTemp();
+	}
 };
 
 var clear = function(){
@@ -24,3 +55,7 @@ var clear = function(){
 
 submitButton.addEventListener('click',calcTemp);
 clearButton.addEventListener('click',clear);
+// degreesNumber.addEventListener('keydown',isEnter);
+degreesNumber.addEventListener('keyup',calcTemp);
+// degreesNumber.addEventListener('submit',calcTemp);
+// document.form.onsubmit = calcTemp();
