062e92744af126a98fa430d3ffd8e5c229c286de
cjyjay35 <cjyjay35@gmail.com>
2016-12-01 08:56:24 -0800

    could not finish it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log('Hello, front end');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOM loaded");
+
+var cel=dcoument.getElementById('C')
+var far=document.getElementById('F')
+
+var ftoc=document.getElementById('ftoc')
+var ctof=document.getElementById('ctof')
+
+function one(){
+  ctof.checked=false;
+}
+
+function two(){
+  ftoc.checked=false;
+}
+
+
+
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+        x = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = (x);
+    } else {
+        x = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = (x);
+    }
+}
