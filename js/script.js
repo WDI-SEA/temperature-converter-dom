56e0f98351b5d28b95f9bee9c68cb5a3b926e648
Tobias Murphy <murphy.tobias@gmail.com>
2016-11-30 22:26:11 -0800

    added html and functioning js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOM loaded");
+});
+
+function fOrC() {
+  if (document.getElementById("F").checked) {
+    console.log("FARANHI");
+    getTempC();
+  } else {
+    console.log("CELSIOR");
+    getTempF();
+  }
+};
+
+
+function getTempC() {
+  var title = document.getElementById("display");
+  var input = document.getElementById("temp");
+
+  var value = temp.value;
+  var result = Math.round((value-32)*(5/9)) + "° C";
+  display.textContent = result;
+}
+
+function getTempF() {
+  var title = document.getElementById("display");
+  var input = document.getElementById("temp");
+
+  var value = temp.value;
+  var result = Math.round((value*(9/5))+32) + "° F";
+  display.textContent = result;
+}
+
+
+document.getElementById("submit").addEventListener("click", function() {
+  console.log("submitted");
+  fOrC();
+  event.preventDefault()
+});
+
+
+
+
+
+
+
+//
+// var convertToC =function (num){
+//   answer = Math.round((num-32)*(5/9)) + "° C";
+//   return answer;
+// };
+//
+// var convertToF =function (num){
+//   answer = Math.round((num*(9/5))+32) + "° F";
+//   return answer;
+// };
