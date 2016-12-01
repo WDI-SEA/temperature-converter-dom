335bfc18e3770a090609bba32c6d1707c0c76446
dmclough <davidclough@gmail.com>
2016-12-01 09:48:30 -0800

    updated output

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 console.log('Hello, front end');
 
-function setInputAsConverted() {
-  var input = document.getElementsById("userInput");
-  var converted = document.getElementById("userOutput");
-
-
-  var converted = input.value;
-
-  userOutput.textContent = converted;
-}
-
-var scale = 0;
-
 function convertToC(f) {
    return (f - 32) * 5/9;
  }
+
 function convertToF(c) {
   return c * 1.8 + 32;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log("DOM loaded");
+
+      var scale = 0;
+      //var input = document.getElementById("userInput");
+      //var temp = input.value;
+      //var userOutput = document.getElementById("userOutput");
 
       document.getElementById("f").addEventListener("click", function() {
           console.log("F radio clicked");
@@ -32,19 +26,44 @@ document.addEventListener('DOMContentLoaded', function() {
           scale = 1;
       })
 
+
+      document.getElementById("clearButton").addEventListener("click", function() {
+          console.log("clear pressed");
+
+          var input = document.getElementById("userInput");
+          var userOutput = document.getElementById("userOutput");
+
+          var converted = "need input";
+          userOutput.textContent = converted;
+          userInput.textContent = converted;
+      })
+
+
+
       document.getElementById("submitButton").addEventListener("click", function() {
           console.log("Submit button clicked");
           switch (scale) {
             case 0:
-              console.log(convertToC(50));
+              console.log("ferenheight running");
+              var input = document.getElementById("userInput");
+              var temp = input.value;
+              var userOutput = document.getElementById("userOutput");
+              var converted = convertToC(temp);
+              userOutput.textContent = converted;
               break;
             case 1:
-              console.log(convertToC(70));
+              console.log("celcius running");
+              var input = document.getElementById("userInput");
+              var temp = input.value;
+              var userOutput = document.getElementById("userOutput");
+              var converted = convertToF(temp);
+              userOutput.textContent = converted;
               break;
             default:
               console.log("something didn't work");
           }
-  });
+      }
+);
 
 
 
