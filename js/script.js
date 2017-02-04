5d8d488bace8fe62e4c41e72f0f4dacd2fe70250
krichdev <kgrichardson2@gmail.com>
2017-02-03 22:23:34 -0800

    Conversion for F to C finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello, front end');
+var degInput = document.getElementById("input");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	document.getElementById("convert").addEventListener("click", function(){
+		var conversion = Math.floor(((degInput.value - 32) * (5/9)));
+		document.getElementById("degreeResult").innerHTML = conversion + " &deg C";
+		document.getElementById("degreeResult").classList.add("display");
+	});
+
+	document.getElementById("reset").addEventListener("click", function(){
+		document.getElementById("degreeResult").innerHTML = "";
+		document.getElementById("degreeResult").classList.remove("display");
+		degInput.value = "";
+	})
+
+});
