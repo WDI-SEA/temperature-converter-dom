0153e5ab4d1723a448c3bebc57501085050113ce
alexandraalday <alexandra.alday@gmail.com>
2017-06-08 21:49:48 -0700

    better design and bonus hot-cold background

--- js/script.js
+++ js/script.js
@@ -1,32 +1,38 @@
 //submit button should calculate converted temperature
 var submitButton = document.getElementById('submit');
 
+//created global variable so all functions had access
+var newTemp = -1;
+
 
 // function to make red or blue background based on temp
  var convertBackground = function(){
 
-  if (document.getElementById('result').value >= 0){
-  		document.getElementById('result').style.backgroundColor = red;
-  } else if (document.getElementById('result').value <= 30){
-  		document.getElementById('result').style.backgroundColor = blue;
+  if (newTemp <= 0){
+  		document.getElementById('result').style.backgroundColor = 'steelblue';
+  } else if (newTemp >= 30){
+  		document.getElementById('result').style.backgroundColor = 'brown';
   } else {
 
   }
 }
 
-// function to calc new temperature
+// function to calculate new temperature
 var convertTemp = function(){
   var temp = document.getElementById('temperature').value;
-  var unit = document.getElementById('F').checked;
+  var celsius = ((temp-32)/(9/5)).toFixed(0);
+  var fahrenheit = ((temp)*(9/5)+32).toFixed(0);
 
   if (temp === "") {
     document.getElementById('result').innerHTML = "Try Again";
-  } else if(unit){
-      document.getElementById('result').innerHTML = ((temp-32)/(9/5)).toFixed(1)+" &deg;C";
+  } else if(document.getElementById('F').checked){
+      document.getElementById('result').innerHTML = celsius +" &deg;C";
+      newTemp = celsius
       convertBackground()
 
   } else {
-      document.getElementById('result').innerHTML = ((temp)*(9/5)+32).toFixed(1)+" &deg;F";
+      document.getElementById('result').innerHTML = fahrenheit +" &deg;F";
+      newTemp = fahrenheit
       convertBackground()
     }
 };
@@ -34,7 +40,7 @@ var convertTemp = function(){
 submitButton.addEventListener('click', convertTemp);
 
 
-//clear button should reset values
+//clear button should reset page
 var clearButton = document.getElementById('clear');
 
 var clear = function(){
