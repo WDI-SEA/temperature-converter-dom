62ee7794265e212a02f19fd87363adf0ec942d31
David Gardner <dpgardne@Davids-MacBook-Pro.local>
2017-06-08 23:39:12 -0700

    hi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+//grab the wraper div
+var result = document.getElementById("wrapper")
+//create a new div
+var resultDiv = document.createElement('div');
+//grab submit button
+var btn = document.getElementById("button1")
+//grab the input
+
+
+
+
+
+//create function for submit button click
+btn.addEventListener('click', function() {
+var = 
+//apend to wrapper
+result.appendChild(resultDiv);
+//style the new wrapper div
+result.style.width = '150px'
+result.style.height = '50px'
+result.style.backgroundColor = 'red'
+
+});
