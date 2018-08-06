e7cdce4eae29a2e7fd22f39da89bbea1a64f35aa
Sky <anvandp18@gmail.com>
2018-08-06 10:04:30 -0700

    not a perfect version

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+console.log('test');
+document.addEventListener('DOMContentLoaded', function(){
+	console.log('nice');
+});
+
+function degreee(){
+	var type;
+	var resultDegree;
+	var deg = document.getElementById('degree1').value;
+
+	var typeFah = document.getElementById('Fah');
+	var typeCel = document.getElementById('Cel');
+
+	if (typeFah.checked){
+		
+		resultDegree = Math.floor((deg - 32)*5/9);
+		type = '°C';
+
+	}else{
+		resultDegree = Math.floor(deg*9/5 + 32);
+		type = '°F';
+	}
+
+	document.getElementById('result').textContent = resultDegree + " " + type;
+	console.log(resultDegree + " " + type);
+	}
