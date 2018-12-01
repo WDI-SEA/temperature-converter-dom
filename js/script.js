731ca15c7b82b0feb80edfdb382b8aae30db87ae
chidrome <chidrome@gmail.com>
2018-11-30 17:37:28 -0800

    https://imgur.com/dBvJsE8

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-console.log('Hello, front end');
+
+
+var faren = document.getElementById("farenheit")
+var cel = document.getElementById("celcius")
+var outPutValue = document.getElementById("outPut");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+
+// This keeps the button from refereshing
+var buttonClick = document.getElementById("temp")
+buttonClick.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+var inputValue = document.getElementById("input").value;
+
+	if(faren.checked){
+		getCelsius(inputValue);
+	}
+	else {
+		getFar(inputValue);
+	}
+
+});
+
+
+function getCelsius(x) {
+  var celsius = ((x-32) * 5 / 9).toFixed(2);
+  outPutValue.innerHTML = celsius + " &#8451;";
+}
+
+function getFar(y) {
+  var far = ((y * 9/5) + 32).toFixed(2);
+  outPutValue.innerHTML = far + " &#8457;";
+}
+
+function clearAll(reset){
+	var value = document.getElementById("input");
+	value.textContent = "";
+	faren.checked = false;
+	cel.checked = false;
+	outPutValue.textContent = "";
+}
+
+var clearButton = document.getElementById("clear")
+clearButton.addEventListener("click", clearAll)
+
+
+
+})
+
+
+
+
+
