c5ea933c16b71feb062892d8c80dfcde69636990
Nhuuu <nqtrinh87@gmail.com>
2018-11-30 16:40:48 -0800

    temp converter completed, but not pretty

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('Hello, front end');
+document.addEventListener("DOMContentLoaded", function(){
+	console.log("dom content has loaded");
+
+
+	var myBtn = document.querySelector('#converter');
+	myBtn.addEventListener('submit',function(e){
+		e.preventDefault();
+
+	    var temp = document.getElementById('tempInput').value;
+		var output = document.getElementById('output');
+
+		if (document.getElementById('F').checked){
+				output.textContent = (((temp - 32) * 5 /9) + " °C");
+
+	    	} else {
+
+	    		output.textContent = (((temp * 9) / 5 + 32) + " °F");
+
+	    }
+})
+})
