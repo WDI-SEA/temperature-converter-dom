f16306a3d942829319782dd280af9c7d1e76b241
JustinRobertKing <kingj4@spu.edu>
2019-07-26 13:54:50 -0700

    meh

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	var buttonS = document.getElementById("submit");
 	var buttonC = document.getElementById("clear");
-	// console.log(tempF)
-	var tempC = 0;
+	var radioF = document.getElementById("radiof");
+	var radioC = document.getElementById("radioc");
+	var tempF = 0
+	var tempc = 0
 	var display = document.getElementById("output");
 	buttonS.addEventListener("click", function(e) {
-		e.preventDefault()
-		var tempF = Number(document.getElementById("input").value);
-		// console.log("hi");
+	e.preventDefault()
+		console.log('c: ', radioC.checked, 'f: ', radioF.checked)
+		tempF = Number(document.getElementById("input").value);
 		tempC = ((tempF - 32) / 1.8).toFixed(2);
 		display.textContent = tempC.toString();
-		console.log(tempC)
 		if (tempC <= 0) {
 			console.log(true)
 			return display.style.backgroundColor = "blue"
@@ -19,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
 			console.log(false)
 			return display.style.backgroundColor = "red"
 		} 
-
 	})
 	buttonC.addEventListener("click", function(e) {
 		e.preventDefault()
