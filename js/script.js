82808803371fe073f17d0669d257e50e8c11ce51
stanstaton <thatkidenzo@gmail.com>
2019-07-25 21:27:51 -0700

    created a temperature converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello, front end');
+document.getElementById('temperature').addEventListener("submit", function(event){
+	event.preventDefault();
+	console.log("Am I understood?");
+	var temp = document.getElementById("box").value;
+	console.log(temp);
+	var newTemp;
+	
+	if (document.getElementById("radio1").checked){
+		newTemp = (temp * 9/5) + 32;
+		console.log("god it works")
+	} else {
+		newTemp = (temp - 32)*(5/9);
+		// console.log("Bwah")
+	};
+	// var x = document.getElementsByTag("p");
+
+	// x.remove(x);
+
+	// var p = document.createElement("p");
+	// p.innerText = newTemp.toFixed(2);
+
+	x = document.getElementById("new");
+	x.innerText = newTemp.toFixed(2);
+	
+
+	document.getElementsByTag("p").append(x.innerText);
+
+})
+
+document.getElementById("clear").addEventListener("click", function(event){
+	location.reload();
+});
