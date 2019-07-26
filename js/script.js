eacd6d09607ca798d7032755b46d84d44c7f1761
pdangelo4 <paulsdangelo@gmail.com>
2019-07-25 21:00:20 -0700

    Temperature Homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+// console.log("Hello, front end");
+
+function tempConvert() {
+    var input = document.getElementById("input").value;
+    var radioForm = document.form.tempChoice;
+    // FConvert = "";
+    // CConvert = "";
+
+    for (var i=0; i < radioForm.length; i++) {
+        if (radioForm[i].checked) {
+            if (radioForm[i].value == "F") {
+                FConvert = (input - 32) * (5 / 9);
+                document.getElementById("output").innerHTML = FConvert + "C";
+            }
+            else if (radioForm[i].value == "C") {
+                CConvert = (input * (9 / 5)) + 32;
+                document.getElementById("output").innerHTML = CConvert + "F";
+            }
+        }
+    }
+};
+
+// function clear()
+// {   
+//    document.getElementById("form").reset();
+// }
