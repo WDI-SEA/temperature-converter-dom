50324636d5c0d73ee1811387a8a8c87fc512487a
michelleurrea <michelleurrea@gmail.com>
2019-07-26 09:23:23 -0700

    Temperature converter homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log('Hello, front end');
+
+function convert(inputF) {
+	console.log("click");
+	var inputF = document.getElementById("userInput").value;
+	//console.log(inputF)
+	// if("celciusButton"==checked){
+	// 	var resultF = (inputF × 9/5) + 32;
+	// }
+	// else {
+	// 	/display result for 
+	// }
+}
+
+/*function clearMethod(clearAll) {
+	console.log("cleared");
+}
+*/
+
+document.getElementById("submit-button").addEventListener("click", convert);
+
+//document.getElementById("clear").addEventListener("click", clearMethod);
