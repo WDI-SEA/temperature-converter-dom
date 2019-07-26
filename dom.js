d2c62499a2b7143a3f3cddc268c651c7bd8d1bef
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-07-26 09:02:09 -0700

    hmwrk3

---
+++ dom.js
@@ -0,0 +1,15 @@
+
+var celToFar = document.getElementById('size').value;
+document.getElementById('Output').innerHTML = celToFar *5/9 + 32;
+
+console.log(celToFar);
+
+var fareToCel = document.getElementById('size').value;
+document.getElementById('Output').innerHTML = (fareToCel - 32) * 5 / 9;
+
+console.log(fareToCel);
+
+
+
+
+
