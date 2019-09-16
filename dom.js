9972ffa11a106d930c2bbcfb00d23b2fdd1aece7
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-09-15 17:01:14 -0700

    finished

--- dom.js
+++ dom.js
@@ -1,13 +1,47 @@
 
-var celToFar = document.getElementById('size').value;
-document.getElementById('Output').innerHTML = celToFar *5/9 + 32;
+var inputBox = document.getElementById("box1")
+var enter = document.getElementById("enter").addEventListener('click', enter);
+var clear = document.getElementById("clear").addEventListener('click', clear);
+var Farren = document.getElementById("Farren");
+var Celcius = document.getElementById("Celcius");
+//getElementById
+	var answer = document.getElementById('answers');
+function enter(){
+	if(Farren.checked){
+		var total = inputBox.value + " % Farrenheit to Celcius is " + (inputBox.value -32) * 5/9 ;
+		answer.innerHTML = total;
+	}
+	else if(Celcius.checked){
+		var total = inputBox.value + " % Celcius to Farrenheit is " + (inputBox.value * 9/5 + 32) ;
+		answer.innerHTML = total;
+	}
+	var fTotal = (inputBox.value -32) * 5/9;
+	var cTotal = (inputBox.value * 9/5 + 32);
+	if(fTotal > 35 || cTotal > 85){
+		document.getElementById("answers").style.color = '#d00';
+	}
+	else if(fTotal < 20 || cTotal < 32){
+		document.getElementById("answers").style.color = 'blue';
+	}
+	
+	
+}
 
-console.log(celToFar);
+function clear(){
+answer.innerHTML= "";
+inputBox.value =0;
+}
 
-var fareToCel = document.getElementById('size').value;
-document.getElementById('Output').innerHTML = (fareToCel - 32) * 5 / 9;
+function getColor(){
 
-console.log(fareToCel);
+
+	
+
+}
+
+
+
+
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log('Hello, front end');
+var inputBox = document.getElementById("box1")
+var enter = document.getElementById("enter").addEventListener('click', enter);
+var clear = document.getElementById("clear").addEventListener('click', clear);
+var Farren = document.getElementById("Farren");
+var Celcius = document.getElementById("Celcius");
+//getElementById
+	var answer = document.getElementById('answers');
+function enter(){
+	if(Farren.checked){
+		var total = inputBox.value + " % Farrenheit to Celcius is " + (inputBox.value -32) * 5/9 ;
+		answer.innerHTML = total;
+	}
+	else if(Celcius.checked){
+		var total = inputBox.value + " % Celcius to Farrenheit is " + (inputBox.value * 9/5 + 32) ;
+		answer.innerHTML = total;
+	}
+	var fTotal = (inputBox.value -32) * 5/9;
+	var cTotal = (inputBox.value * 9/5 + 32);
+	if(fTotal > 35 || cTotal > 85){
+		document.getElementById("answers").style.color = '#d00';
+	}
+	else if(fTotal < 20 || cTotal < 32){
+		document.getElementById("answers").style.color = 'blue';
+	}
+	
+	
+}
+
+function clear(){
+answer.innerHTML= "";
+inputBox.value =0;
+}
+
+function getColor(){
+
+
+	
+
+}
