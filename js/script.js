ae5334bf3bbe3dcc99d512831078677216d78a67
garedb <garedbarney@gmail.com>
2020-03-20 08:42:19 -0700

    Temp Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', function() {
+  let tempertureForm = document.getElementById('temperatureForm');
+  let temperature = document.getElementById('temperature');
+  let tempDiv = document.getElementById('tempDiv');
+  let reset = document.getElementById('reset');
+
+  temperatureForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let convertedTemp = (temperature.value - 32) * (5 / 9);
+    tempDiv.innerHTML = convertedTemp.toFixed(2);
+  });
+});
