e192e08f2bab61f2c09cf35678d2fd5857996cea
Chuck Wagon <chuckwagon@Chucks-MacBook-Air.local>
2020-03-19 19:08:36 -0700

    YOU NEED MORE TOILET PAPERgit status

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello, front end');
+console.log('hey im js, and im working!')
+document.getElementById('submit').addEventListener('click', (e) => {
+    console.log('submitting your number now!')
+    // console.log(document.getElementById('user-input').value)
+    let startValue = parseInt(document.getElementById('user-input').value)
+    let resultValue = startValue + 4245010818
+    document.getElementById('display-result').textContent = resultValue
+})
