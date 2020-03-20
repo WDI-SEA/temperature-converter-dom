d16728c015f7be7255ee97aae4f94bf013c3d198
gjones81 <jones.guy81@gmail.com>
2020-03-19 20:49:44 -0700

    homework completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello, front end');
+document.getElementById('submit').addEventListener('click' , (e) => {	
+	let tempInput = parseInt(document.getElementById("user-input").value)	
+	let convertedTemp = (((tempInput - 32) * 5) / 9)
+	let roundedConvertedTemp = Math.round(convertedTemp)
+	document.getElementById('result').textContent = roundedConvertedTemp
+	if (roundedConvertedTemp > 100) {
+		document.getElementsByTagName("div") = 'red'
+	} 
+	else if (roundedConvertedTemp < 0) {
+		document.getElementsByTagName("div") = 'blue'
+	}
+	else {
+		document.getElementsByTagName("div") = 'white'
+	}
+})
+
+
+document.getElementById('clear').addEventListener('click' , (e) => {
+	document.getElementById('result').textContent = 'Try Another One'
+})
