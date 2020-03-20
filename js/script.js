4b13522ebaf2528e24c5fa31fc67b5914edf4e27
ObtuseSpruce <williamllapitan@gmail.com>
2020-03-20 08:45:54 -0700

    changed css, changed js temp formula

--- js/script.js
+++ js/script.js
@@ -10,17 +10,20 @@ let displaytemp = document.getElementById('giveMeTemp2');
 let convertEvents = () => {
     let currentTemp = displaytemp.value;
     let celTrue = document.getElementsByName('temp');
-    // console.log(celTrue);
-    // console.log("0 index:", celTrue[0].checked);
+    console.log(celTrue);
+    console.log("0 index:", celTrue[0].checked);
     // console.log("1 index:", celTrue[1].checked);
     if (celTrue[1].checked) {
-    display.textContent = (currentTemp - 32) / 2;
+        display.textContent = (currentTemp - 32) / 2;
+    } else if (celTrue[0].checked) {
+        display.textContent = Math.floor((currentTemp - 32) * (5/9) - 273);
     } else {
-    display.textContent = currentTemp;
+        display.textContent = currentTemp;
     }
 }
 
 let clearEvents = () => {
+        let cleardisplay = displaytemp.value;
         display.textContent = '';
 }
 
