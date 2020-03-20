723cc57ecaad854cff7e082348a1bcec4cdb2b30
smcleod86 <mcleod.scott_86@yahoo.com>
2020-03-20 06:53:04 -0700

    converts temperature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello, front end');
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('clear').onclick = clearForm;
+
+let temp = ""
+let c = (temp - 32) * (5/9)
+
+console.log(temp + ' degrees Fahrenheit is ' + c + ' degrees Celcius.')
+
+if 
