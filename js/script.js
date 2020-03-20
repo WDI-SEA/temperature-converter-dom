63f88816673fa6646c6806291fc0ecc2ca4a4f04
spazevic <spazevic@yahoo.com>
2020-03-19 22:49:14 -0700

    got working converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('Hello, front end');
+
+	let displayBox = document.getElementById('display');
+	let tempInput = document.getElementById('tempIn');
+	let submit = document.getElementById('submitButton');
+	let clear = document.getElementById('clearButton');
+
+
+
+let doWork = (temp) => {
+	
+
+	if (document.getElementById('celcius').checked == true) {
+		temp = (temp - 32)/1.8
+	}
+	else {
+		temp = (temp * 1.8) + 32;
+	}
+    displayBox.textContent = temp;
+
+}
+clear.addEventListener('click', (e) => {
+	displayBox.textContent = ''
+	tempInput.value = ' ';
+})
+
+submit.addEventListener('click', (e) => {
+	doWork(tempInput.value);
+})
+
