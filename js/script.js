d1cca7866b63a6883d7c3f117d0bcd79fa5b2ebb
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-03-20 08:37:42 -0700

    created temp template

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, front end');
+
+	let Output = document.getElementById('output');
+	let input = document.getElementById('temp');
+	let s = document.getElementById('submit');
+	let c = document.getElementById('clear');
+
+
+
+let record = (x) => {
+	if (document.getElementById('C').checked == true) {
+		x = (x - 32)/1.8
+	}
+	else {
+		x = (x * 1.8) + 32;
+	}
+
+ Output.textContent = "x";
+
+}
+c.addEventListener('click', (event) => {
+	output.textContent = ''
+	input.value = ' ';
+});
+
+s.addEventListener('click', (event) => {
+	record(input.value);
+})
