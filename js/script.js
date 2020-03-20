779166411934d6887daa5388cdc746a196f24883
essleeung <essleeung@gmail.com>
2020-03-20 10:38:21 -0700

    switched to using the radio button values like I originally wanted

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
 let temp = document.getElementById('tempInput');
-// let indicator = document.querySelector('input[name="tempType"]:checked');
 let submitBtn = document.getElementById('submit');
 let clearBtn = document.getElementById('clear');
 let display = document.getElementById('display');
-let radioBtns = document.getElementsByClassName('tempType');
+// let radioBtns = document.getElementsByClassName('tempType');
 
-// // const convert = () => {
-// //     if (indicator.value === 'F') {
-// //         console.log (temp.value - 32) * (5/9);
-// //     } else if (indicator.value === 'C') {
-// //         console.log (temp.value * 9/5) + 32;
-// // }    
-
-//Which coversion based on radio button
-let convert = (temp) => {
-    //convert sFtoC
-    if(radioBtns[0].checked === true) { 
-        var newTemp = Math.round((temp - 32) * (5/9));
+const convert = (temp) => {
+    let indicator = document.querySelector('input[name="tempType"]:checked')
+    if (indicator.value === 'F') {
+        var newTemp = Math.round((temp - 32) * (5 / 9));
         display.textContent = newTemp + "\xB0C";
-    } else {
-        var newTemp = Math.round((temp * 9/5) + 32);
+    } else if (indicator.value === 'C') {
+        var newTemp = Math.round((temp * 9 / 5) + 32);
         display.textContent = newTemp + "\xB0F";
     }
 }
+//Which coversion based on radio button
+// let convert = (temp) => {
+//     //convert sFtoC
+//     if(radioBtns[0].checked === true) { 
+//         var newTemp = Math.round((temp - 32) * (5/9));
+//         display.textContent = newTemp + "\xB0C";
+//     } else {
+//         var newTemp = Math.round((temp * 9/5) + 32);
+//         display.textContent = newTemp + "\xB0F";
+//     }
+// }
+
+//Using radiobtn for submit
+// submitBtn.addEventListener("click", (e) => {
+//     convert(temp.value);
+// });
 
 submitBtn.addEventListener("click", (e) => {
     convert(temp.value);
