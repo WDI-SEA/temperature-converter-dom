eafac5e1989ae0b22efa428132b17c99daeb218c
royce-u <royceubando@gmail.com>
2020-03-20 08:05:02 -0700

    hw done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello, front end');
+// console.log(document.getElementById('tempInput').value);
+// document.getElementById('tempInput') 
+
+let tempInput = document.getElementById('tempInput')
+var submit = document.getElementById('submit')
+var clear = document.getElementById('clear')
+var res = document.getElementById('result')
+var cel = document.getElementsByClassName('btn')
+    
+function converter(temp) {
+    if (cel[1].checked == true) {
+        var result = (temp - 32) * 5/9
+        res.textContent = result
+    }
+    else {
+        var result = (temp *= 9/5) + 32
+        res.textContent = result
+    }
+}
+submit.addEventListener('click', (e) => {                                
+    converter(tempInput.value)
+
+})
+clear.addEventListener('click', (e) => {
+    console.log('fff')
+    tempInput.value = ''
+    res.textContent = '';
+});
