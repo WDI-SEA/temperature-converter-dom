6d94f088ffa568ea47b24160bee432f99005a532
smcleod86 <mcleod.scott_86@yahoo.com>
2020-03-20 11:39:49 -0700

    Temp after Nicks help

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear').onclick = clearForm;
 
-let temp = ""
-let c = (temp - 32) * (5/9)
+let fahr = document.getElementById("Fahrenheit")
+let cels = document.getElementById("Celcius")
+let tempConvt = document.getElementById("convert")
+let reset = document.getElementById("clear")
 
-console.log(temp + ' degrees Fahrenheit is ' + c + ' degrees Celcius.')
 
-if 
+console.log(fahr)
+//Convert the temperature
+function tempConvert() {
+    var fahrenheit = document.getElementById("Fahrenheit").checked;
+    let input = document.getElementById("temp").value
+    if (fahrenheit) {
+      console.log(input)
+        celciusInput = (parseInt(input) - 32) / (5 / 9);
+        document.getElementById("converted").textContent = celciusInput
+    } else {
+        console.log("hitting the else")
+        fahrenheitInput = (parseInt(input) * (5 / 9) + 32);
+        document.getElementById("converted").textContent = fahrenheitInput
+    }
+}
+
+// Clear button
+function clearForm() {
+    document.getElementById('converted').textContent = '';
+    document.getElementById('temp').value = '';
+} 
+
+reset.addEventListener("click", clearForm)
+tempConvt.addEventListener("click", tempConvert)
