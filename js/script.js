cec6d8fefdb3e6fd1b28fb514e7c4251636c1b36
ObtuseSpruce <williamllapitan@gmail.com>
2020-03-19 21:01:43 -0700

    very rough functional site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('Hello, front end');
+
+let convertButton = document.getElementById('convert');
+let clearButton = document.getElementById('clear');
+let display = document.getElementById('display1');
+let displaytemp = document.getElementById('giveMeTemp2');
+
+
+
+let convertEvents = () => {
+    let currentTemp = displaytemp.value;
+    let celTrue = document.getElementsByName('temp');
+    // console.log(celTrue);
+    // console.log("0 index:", celTrue[0].checked);
+    // console.log("1 index:", celTrue[1].checked);
+    if (celTrue[1].checked) {
+    display.textContent = (currentTemp - 32) / 2;
+    } else {
+    display.textContent = currentTemp;
+    }
+}
+
+let clearEvents = () => {
+        display.textContent = '';
+}
+
+convertButton.addEventListener('click', convertEvents)
+clearButton.addEventListener('click', clearEvents)
