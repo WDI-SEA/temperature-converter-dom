c4bacd85ca9596a0e6ecb82a8dae4abe15e8690e
Hilary Lawlor <hilaryrlawlor@gmail.com>
2020-05-28 20:16:04 -0700

    Temperature Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello, front end');
+document.getElementById("tempInput").addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+var results = document.querySelector(".results");
+
+function buttonClick(e) {
+  var number = document.getElementById("temperature").value;
+  var answer =  (number - 32)*(5/9);
+  results.innerText = answer;
+}
+
+function clearButtonClick(e) {
+  document.getElementById("tempInput").reset();
+}
+
+
+document.getElementById("buttonSubmit").addEventListener("click", buttonClick);
+document.getElementById("buttonClear").addEventListener("click", clearButtonClick);
