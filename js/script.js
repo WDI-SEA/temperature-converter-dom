a3dab0b64300b3cf4c67124bd4dc00edf4ef8e53
jdephil <jennifer.dephillips@gmail.com>
2020-05-28 23:23:56 -0400

    created temp converter webpage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello, front end');
+
+var resultDiv = document.querySelector(".result")
+var submit = document.getElementById('submitButton')
+var clear = document.getElementsByClassName('clearButton')
+
+
+submit.addEventListener('click', function(e) {
+    var userTemp = (document.getElementById('userTemp').value)
+    var celsius = ((userTemp - 32) * .5556)
+    resultDiv.innerText = (celsius + " degrees celsius")
+})
+
+document.getElementsByClassName('clearButton').onclick = clearResults
+
+function resetForm() {
+    document.getElementById('userTemp').value = ('');
+    resultDiv.innerText = ('');
+}
+
+resetForm ();
