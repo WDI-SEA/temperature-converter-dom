8ce8ccca6f6880f48e1a6e6a803266f72ed0e806
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-05-29 10:09:58 -0600

    added celsius and list view

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, front end');
+
+function convert() {
+    let input = document.getElementById('temp').value;
+    console.log(input)
+    let fahrenheit = Math.round((input * (9/5)) + 32);
+    console.log(fahrenheit);
+    return  display(fahrenheit);
+    
+    
+}
+
+function display(){
+    let input = document.getElementById('temp').value;
+    let fahrenheit = Math.round((input * (9/5)) + 32)
+    let listItem = document.createElement('li');
+    listItem.textContent = `is ${fahrenheit} degrees fahrenheit`;
+    document.getElementById('history').prepend(listItem)
+}
+
+
+
+
+document.getElementById("submit").addEventListener ("click", ()=> convert());
+
+
+
+
