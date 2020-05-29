6894ed90a7a16a50160cd34c60ca369128012356
d4vves <d4vves@gmail.com>
2020-05-28 22:29:30 -0500

    updated index, css and js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello, front end');
+// temperature conversion submit
+var convertTemp = document.querySelector(".submitButton");
+convertTemp.addEventListener("click", function(e) {
+    function temperature(value) {
+        return (((value - 32) * 5) /9).toFixed(0) + "°C";
+    }
+    var result = temperature(document.querySelector(".tempInput").value);
+    var resultText = document.querySelector(".resultHeader");
+    resultText.innerText = result;
+    var resultColor = document.querySelector(".bigContainer");
+    if (document.querySelector(".tempInput").value >= 80) {
+        resultColor.style.backgroundColor = "#b23a48";
+    } else if (document.querySelector(".tempInput").value <= 32) {
+        resultColor.style.backgroundColor = "#008dd5"
+    } else {
+        resultColor.style.backgroundColor = "#ffecd1"
+    }
+})
+
+//clear button sweet action
+var clearTemp = document.querySelector(".clearButton");
+clearTemp.addEventListener("click", function(e) {
+    document.querySelector(".resultHeader").innerText = null;
+    document.querySelector(".tempInput").value = null;
+})
