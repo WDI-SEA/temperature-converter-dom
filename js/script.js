16149e546e3e2ede8e97a3c4361c83ba8bdf1002
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-05-29 11:13:29 -0500

    temperature converter celsius to fahrenheit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello, front end');
+var toCelsius = document.querySelector("#submit");
+toCelsius.addEventListener("click", function(event){
+    var input = document.querySelector("#inputBox").value;
+    var solution = document.querySelector("#convertedValue").innerHTML = 
+    solution = ((input * (9/5)) + (32) + "°F");
+})
+
