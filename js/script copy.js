6040f5d01d75dc2aea0a3c3a304ac179214ee918
feelivey9196 <feelivey9196@gmail.com>
2020-05-28 22:14:56 -0700

    RYAN SHIN HW 3

---
+++ js/script copy.js
@@ -0,0 +1,8 @@
+//When the submit button is pressed,
+
+//Get a reference to the submit button.
+// add an event listener
+
+//convert the inputted temperature from F to C using the appropriate conversion formula (may need to Google it)
+
+//Updated with the converted temperature value.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello, front end');
+
+function convertTemp(){
+    event.preventDefault(); 
+    var inputVal = document.getElementById("user-input").value;
+    var outputVal = (inputVal - 32) * (5/9);
+    document.getElementById("user-output").value = outputVal;
+}
+
+document.getElementById("submit").onclick = convertTemp;
