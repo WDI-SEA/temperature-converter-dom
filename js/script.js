6a1b925b9b8080ce8c0517c2bf00f027e6c12b43
sky777666 <lmyer12@my.wgu.edu>
2020-05-29 07:10:06 -0700

    Luke Myers tempCal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
-console.log('Hello, front end');
+console.log('Hello, Temp Conversion);
+
+
+// formulas ------ working code placed into html file as script becasue this will not link/work//
+var convertF = function(num) {
+    return (num -32) / 1.8
+}
+
+var convertC = function(num) {
+    return (num * (9/5))  32 ;
+
+}
+
+// Radio Selection buttons //
+var userChoiceF =document.getElementById("f"). addEventListener("click"), (e) =>{
+    convertF();
+
+var userChoiceC = document.getElementById("c"). addEventListener("click"), (e) =>{
+    convertC();
+});
+
+let temp = 0
+
+function updatteDisplay(){
+    temp = document.getElementById()"temp-entry".value)
+    document.getElementById("temp-display").innerText = temperature
+    }
+}
+
+// Not sure about this code -----NOT working---------------------------------------//
+
+// Output //
+  function showCelsiusConversion(){
+    var f = parseFloat(document.getElementById('fahren').value);
+
+function myFunction () {
+    document.getElementById("myForm").requestFullscreen();
+}
+
+
+var result = document.getElementById('box');
+}
+
+//--------------------//
+var Fahr2Cels = document.getElementById('Submit1');
+
+
+// Referance for working code is https://www.w3schools.com/howto/howto_js_temperature_converter.asp //
+
+// Referance for Radio Switch is https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_radio //
+
+// Referance for Working JS https://codepen.io/mmlumba/pen/bYMRdO //
+
+// I could not get any of this to work //
