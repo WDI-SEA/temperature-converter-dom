26c503d01d3a340da35777a89cf87303c2e97129
nguyendm77 <nguyendm77@gmail.com>
2020-05-29 09:39:38 -0700

    submit DOM hw

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 
-document.getElementById('submit').addEventListener('click', (e) => {
-  console.log(document.getElementById('entry').value);
+let submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", function(event) {
+  let inputVal = document.querySelector("#inputText").value
+  let fahrenheit = document.getElementById("#tempF");
+  let celsius = document.getElementById("#tempC");
+    if (fahrenheit.checked == true) {
+      inputVal = (inputVal - 32) * 5/9 ;
+    } else if (celsius.checked == true) {
+      inputVal = ((inputVal/5)*9)+32 ;
+    } else {
+    alert ("Please select temperature unit first!");  
+    } 
+  document.getElementById("#result").value = inputVal;
 });
 
-document.getElementById('using').addEventListener('submit', (e) => {
-  e.preventDefault();
-  console.log('The form has been submitted');
-
+let clearBtn = document.querySelector("#clear");
+document.addEventListener("click", function(event) {
+    document.querySelector("#result").innerText = "";
 });
