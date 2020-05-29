a0c88ffc17a5a6b4d749d576b4cd61f440c53849
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-05-29 08:10:35 -0700

    Cited Temp Converter

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@ var temp = document.getElementById("#temp")
 var entry = document.getElementById("#Entry")
 
 /*function that will listen for an entry and submission, it should take the text submitted and spit it into the conditionals below */
+/*learned this while on the TA hours with Yoshi*/ 
 
 document.getElementById('#entry').addEventListener('click', (e) => {
     e.preventDefault()
     console.log(document.getElementById('#submit')value)})
+
 /*conditionals that should return the value if you select either the F or C radio button */ 
+/*learned this while on the lab with Pete */ 
 
 if (farenheit.checked === true){
 let temp = document.querySelector(".radio");
