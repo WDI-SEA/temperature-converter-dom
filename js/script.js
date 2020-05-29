2b8bc8bfb0787f8fa960cb9a3f97afae1c449750
Nick Hvattum <nhvattum@gmail.com>
2020-05-29 08:55:14 -0700

    temperature converter completed homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello, front end');
+document.getElementById("submit").addEventListener("click", function(e){
+    var temperature = document.getElementById("entry").value;
+    var highTemp = (parseInt(temperature) - 32) * (5/9);
+    console.log(highTemp);
+    document.getElementById("results").innerText=highTemp + " °C"
+
+})
+
+
+var clearFields = document.getElementById("clear")
+
+function clearInputField() {
+    document.getElementById("results").innerText = null;
+    document.getElementById("entry").value = null;
+}
+
+clearFields.addEventListener("click", function(e) {
+    clearInputField()
+})
+
