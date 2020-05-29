24235a81f526c41e1ddf58b917c9d8eba0d67f6c
nguyendm77 <nguyendm77@gmail.com>
2020-05-28 19:14:39 -0700

    implement code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello, front end');
+
+document.getElementById('submit').addEventListener('click', (e) => {
+  console.log(document.getElementById('entry').value);
+});
+
+document.getElementById('using').addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('The form has been submitted');
+
+});
