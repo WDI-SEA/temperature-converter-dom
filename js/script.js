d0c78276bf0a971611311a35bedf1d68cf21c1f8
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-05-29 08:49:13 -0700

    NessoTemp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello, front end');
+var convertTemp = document.querySelector(".submit");
+convertTemp.addEventListener("click", function(e) {
+    e.preventDefault();
+    function temperature(value) {
+        return ((value - 32) *5) /9;
+    }
+    result = temperature(document.querySelector(".input").value);
+    //so i wanted t use convertedTemp but that was already used and i guess you can use it here again either :(
+    var resultTemp = document.createElement("h2");
+    resultTemp.className = "Converter_Temp" 
+    resultTemp.innerText = result;
+    document.body.appendChild(resultTemp);
+})
