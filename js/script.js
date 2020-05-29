7bf4ddcfa315a45d934b0d0a1a38b6f0fd793452
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-05-29 11:13:34 -0600

    cleaned code

--- js/script.js
+++ js/script.js
@@ -4,25 +4,23 @@ function convert() {
     console.log(input)
     let fahrenheit = Math.round((input * (9/5)) + 32);
     console.log(fahrenheit);
-    return  display(fahrenheit);
+    return display(fahrenheit);
     
     
 }
 
-function display(){
-    let input = document.getElementById('temp').value;
-    let fahrenheit = Math.round((input * (9/5)) + 32)
+function display(fahrenheit){
     let listItem = document.createElement('li');
     listItem.textContent = `is ${fahrenheit} degrees fahrenheit`;
     document.getElementById('history').prepend(listItem)
 }
-
 function removeList(){
     var itemToRemove = document.getElementById('history');
     itemToRemove.parentNode.removeChild(itemToRemove);
 }
 
 document.getElementById("clear").addEventListener ("click", ()=> removeList())
+
 document.getElementById("submit").addEventListener ("click", ()=> convert());
 
 
