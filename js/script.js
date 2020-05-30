f8752b3c4db6f797729ed3d53ae2b486078b6480
Charles Perry <charles@Charless-MacBook-Pro.local>
2020-05-29 17:47:09 -0700

    tired my best with what I knew and could find on the internet

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log('Hello, front end');
+//Set value of temp to that of user input
+var tempresult = document.querySelector(".output");
+var form = document.querySelector("form");
+var temp = form.elements["#user-input"].value;
+// const fradio = document.getElementById("f")
+// const cradio = documnet.getElementById("c")
+// function clearFeild() {
+//     document.getElementById("sub").reset();
+// }
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+})
+form.elements("submit").addEventListener("submit", function(e) {
+    if (form.elements["f"].checked === true) {
+        var convertedNum = (temp - 32) * (5/9);
+        tempresult.innerText = convertedNum;
+    } else if (form.elements["c"].checked === true) {
+        var convertedNum = (temp * 1.8) + 32;
+        tempresult.innerText = convertedNum;
+    } else {
+        tempresult.innerText = "Please select if you 'F' or 'C'";
+    }
+})
+
+
+// document.getElementById("Clear").addEventListener("click", clearFeild());
+// function Calculate(){
+
+//}
+// I stole this almost directly from Annas mock, and frankly need it explained better
+form.elements["clear"].addEventListener('click', fuction(e) {
+    form.elements["input"].value = "";
+    tempresult.innerText = "";
+})
