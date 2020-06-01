ca8b9532d7e36fc39691a287741b417b256fde3e
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-06-01 10:07:02 -0700

    Temp Homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello, front end');
+var result = document.getElementById('result');
+var fahrenheit = document.getElementById('fahrenheit');
+var celsius = document.getElementById('celsius');
+var input = document.getElementById('number').value;
+
+document.addEventListener("submit", function(e) {
+e.preventDefault()
+    if(fahrenheit = (input - 32) * (5/9)) {
+       result.innerText = fahrenheit;  
+    } else {
+        celsius = (input * 1.8) + 32;
+        result.innerText = celsius;
+    }
+    
+    })
+        document.getElementById("clear").addEventListener("click", function(e) {
+        document.getElementById("number").value = "";
+        result.innerHTML = "";
+       
+
+    })
+    
+
+   
+   
+   
+    
+
+
+
+
+
