4e83a80ddb9dee0ab11ff9acbafc1300c7f01e3d
Nick Foxen <foxen.nicholas@gmail.com>
2020-06-19 18:07:13 -0700

    Complete forgot to commit and push this homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log('Hello, front end');
+// Create the variables needed
+let temperature = 0;
+let celsiusButton = document.getElementById("convertToC");
+let kelvinButton = document.getElementById("convertToK");
+let temperatureDisplay = document.getElementById("temperature");
+let userNumber = document.getElementById("amount");
+ 
+
+
+// Functions and there on click events
+
+
+const convertToC = () => {
+  temperature = Math.floor((parseInt(userNumber.value) - 32) * (5/9));
+  temperatureDisplay.innerText = `${temperature}°C`;
+  if (temperature < 0) {
+    temperatureDisplay.style.color = "blue";
+  } else if (temperature > 45) {
+    temperatureDisplay.style.color = "red";
+  } else {
+    temperatureDisplay.style.color = 'white';
+  }
+}
+
+const convertToK = () => {
+  temperature = Math.floor((parseInt(userNumber.value) + 459.67) * (5/9));
+  temperatureDisplay.innerText = `${temperature}°K`;
+  if (temperature < 274) {
+    temperatureDisplay.style.color = "blue";
+  } else if (temperature > 321) {
+    temperatureDisplay.style.color = "red";
+  } else {
+    temperatureDisplay.style.color = 'white';
+  }
+}
+
+celsiusButton.addEventListener("click", convertToC);
+kelvinButton.addEventListener("click", convertToK);
