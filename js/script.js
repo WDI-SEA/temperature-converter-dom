8813a6c93f2833d73f383f42cb28c300a36ab43b
huiylu <74116599+huiylu@users.noreply.github.com>
2020-12-16 21:15:54 -0800

    Nearly done Styling

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
         var temp=0
         if(conv){
             temp = (tempValue -32)*(5/9);
-            document.getElementById("output1").innerText="F";
+            document.getElementById("output1").innerText="C";
         } else {
             temp = tempValue*9/5+32;
-            document.getElementById("output1").innerText="C";
+            document.getElementById("output1").innerText="F";
         }
         document.getElementById("output").innerText=Math.round(temp);
         //document.getElementById("feedback").innerText=res
