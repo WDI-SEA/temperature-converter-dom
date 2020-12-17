4279b20a212d7a8e69fdaf01f979e6cdc9198513
damatoaj <mr.arthurdamato@gmail.com>
2020-12-17 08:51:57 -0800

    fifth

--- js/script.js
+++ js/script.js
@@ -20,4 +20,12 @@ celcius.addEventListener("click", (e) =>)
 var f == ((x - 32) * 5556) && #F
 var c == ((x * 1.8) + 32) && #C
 
+// attempted if/then
 
+if tempInput && f && submitButton{
+
+}
+
+if tempInput && c  && submitButton{
+
+}
