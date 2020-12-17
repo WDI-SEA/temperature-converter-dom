81759388687986daab6de3254ae9c26556f3d367
huiylu <74116599+huiylu@users.noreply.github.com>
2020-12-16 19:45:18 -0800

    v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log("Hi")
+    document.querySelector("input[type=submit]")
+    submitButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+        //let res= document.getElementById("temp").value;
+        let conv=document.getElementById("F").checked;
+        let tempValue=document.getElementById("submitText").value
+        var temp=0
+        if(conv){
+            temp = (tempValue -32)*(5/9);
+            document.getElementById("output").innerText=temp;
+        } else {
+            temp = tempValue*9/5+32;
+        }
+        
+        //document.getElementById("feedback").innerText=res
+        
+        console.log(temp);
+        
+    })
+    
+})
