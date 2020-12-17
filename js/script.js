79c519b7d6754d22fddc3dcaa4503c943371b0dc
JJDev <dancesfire@gmail.com>
2020-12-17 09:41:44 -0800

    Tried to add radio buttons, got stuck. Have better understanding after Taylor review, but moving to loops now

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 document.querySelector("form").addEventListener("submit", (e)=>{
-    e.preventDefault()
-    document.querySelector("#practice").value
-    let temp =((practice.value-32)*.5556)
+    e.preventDefault(); 
+    if document.getElementById('fahrenheit').onclick = true {
+        let tempType = 1.8 + 32;
+    } else if document.getElementById('celsius').onclick {
+        let tempType = .5556 - 32;
+    }
+    document.querySelector("#tempForm").value
+    let temp =((tempForm.value*tempType)
     //console.log(document.querySelector("#practice").value + temp)
     return document.querySelector(".result").innerHTML = temp 
+    }
+
+        // var fer = document.getElementById('fahrenheit');
+        // var cels = document.getElementById('celsius');
+        // var ex3 = document.getElementById('example3');
+
+        // if fer.onclick = tempType =1.8 + 32;
+        // cels.onclick = handler;
+        // ex3.onclick = handler;
+
     
-    console.log(temp)
-    let cel = ((practice.value-32)*.5556)
-    console.log(e.target)
-    })
+    // console.log(temp)
+    // let cel = ((practice.value-32)*.5556)
+    // console.log(e.target)
+    }
    //let yourAnswer = prompt('Whats the Temperature in Ferhinhight?', 'Yes/No');
 
 // let cel = ((#practice.value-32)*.5556)
