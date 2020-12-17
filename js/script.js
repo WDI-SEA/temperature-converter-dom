e68367993c8d2712ff332104a0fab3b79e963a8b
damatoaj <mr.arthurdamato@gmail.com>
2020-12-16 20:58:08 -0800

    second

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const tempInput = document.getElementById("temps")
 
+let submitButton =document.querySelector("#submit")
+submitButton.addEventListener("click", (e)=> ) {
+
+}
+let clearButton = document.querySelector("#clear")
+clearButton.addEventListener("click," (e) =>)
 
 
-
-
-
-
+let solution = document.getElementById("solution")
 
