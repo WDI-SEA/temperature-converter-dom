13e1c29adc3778264979bf725d7d6592d8b68a86
Blake DeGraw <bbdegraw@gmail.com>
2020-12-16 20:40:56 -0800

    uploaded working temp converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello, front end');
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+   
+    let tempIn = document.getElementById('box1')
+    
+    tempIn.innerText = document.querySelector('#tempInput').value
+  
+    let tempOut = document.getElementById('box2')
+    
+    let value = document.querySelector('#tempInput').value
+    
+    tempOut.innerText = (parseInt(value) - 32) * 0.5556
+
+  })
+  
+  document.querySelector('form').addEventListener('reset', (e) => {
+    
+    
+    let tempIn2 = document.getElementById('box1')
+    
+    tempIn2.innerText = "F"
+    
+    let tempOut2 = document.getElementById('box2')
+    
+    tempOut2.innerText = "C"
+  })
+
+  
