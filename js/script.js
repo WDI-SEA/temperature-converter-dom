af47db537ed9a00e2efb88b36cdcc0c6904d8a2b
ElyssaW <ejwinch1@gmail.com>
2020-12-16 22:38:12 -0600

    Added ability to switch input temperature measurement

--- js/script.js
+++ js/script.js
@@ -5,16 +5,44 @@ document.addEventListener('DOMContentLoaded', ()=>{
         e.preventDefault();
         let tempInput = document.querySelector('#temp').value
         
+        
+        let startInput = document.querySelector("input[name='start-temp']:checked").value
         let currentTemp = document.querySelector("input[name='temp-radio']:checked").value
         
-        if (currentTemp === "c") {
-            output.innerText = "Output: " + Math.round((tempInput-32)*5/9) + "C";
-        } else if (currentTemp === "f") {
-            output.innerText = "Output: " + Math.round((tempInput-32)*5/9) + "C";
-        } else if (currentTemp === "k") {
-            output.innerText = "Output: " + Math.round((tempInput-32)*5/9+273.15) + "K";
+        if (isNaN(tempInput)) {
+            output.innerText = "Please input a valid number.";
         } else {
-            output.innerText = "Please select a temperature to convert to!";
+            switch(startInput) {
+             case "f":
+                if (currentTemp === "f") {
+                    output.innerText = "Output: " + tempInput + "F";
+                } else if (currentTemp === "c") {
+                    output.innerText = "Output: " + Math.round((tempInput-32)*5/9) + "C";
+                } else if (currentTemp === "k") {
+                    output.innerText = "Output: " + Math.round((tempInput-32)*5/9+273.15) + "K";
+                } 
+            break;
+            
+            case "c":
+                if (currentTemp === "c") {
+                    output.innerText = "Output: " + tempInput + "C";
+                } else if (currentTemp === "f") {
+                    output.innerText = "Output: " + Math.round((tempInput*9/5)+32) + "F";
+                } else if (currentTemp === "k") {
+                    output.innerText = "Output: " + Math.round(tempInput+273.15) + "K";
+                } 
+            break;
+                
+            case "k":
+                if (currentTemp === "c") {
+                    output.innerText = "Output: " + Math.round(tempInput-273.15) + "C";
+                } else if (currentTemp === "f") {
+                    output.innerText = "Output: " + Math.round((tempInput-273.15)*9/5+32) + "F";
+                } else if (currentTemp === "k") {
+                    output.innerText = "Output: " + tempInput + "K";
+                } 
+            break;
+            }
         }
         
         
