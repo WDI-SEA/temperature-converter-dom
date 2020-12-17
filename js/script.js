ba4617b837762aa7a500eddd30d849983edb54ed
Kenny Jean <kennyjean561@gmail.com>
2020-12-17 10:04:31 -0600

    implimenting JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    console.log("DOM has loaded")
-})
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     console.log("DOM has loaded")
+// })
 
-console.log('Hello, front end');
+// let tempConvert = document.getElementById('temp') ()=>{
+//     console.log(tempConvert)
+// } 
+
+
+let temp = document.getElementById('#temp')
+document.querySelector("input.temp").addEventListener('input', function (e) {
+    e.preventDefault();
+    console.log('temp')
+
+});
+
+
+        
+
+
+
+
