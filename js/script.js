59450b22906ed97e08860d38f706d1c0c448eee7
Your Name <you@example.com>
2020-12-16 19:52:13 -0800

    Built out HTML a little more, and basic colors in CSS.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', () => { // Waits for the page to load before running script.
+
+}
+
+
+// Winter background: https://cdn.pixabay.com/photo/2017/12/02/17/47/wintry-2993370_1280.jpg
+// Desert background: https://cdn.pixabay.com/photo/2017/09/22/09/48/desert-2774945_1280.jpg
+// Blue sky background: https://cdn.pixabay.com/photo/2017/03/22/17/40/hill-2165759_1280.jpg
