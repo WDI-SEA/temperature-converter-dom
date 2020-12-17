5d5f519e522a38b8387c67333f94e2b7247745f2
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2020-12-16 22:48:49 -0800

    temperature conversion assignment

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello, front end');
+document.querySelector("#submit").addEventListener('click', (e)=> convert(e))
+
+document.querySelector("#clear").addEventListener('click', convert)
+
+let output = document.getElementById("output")
+
+let tempInput = document.getElementById("tempInput")
+
+function convert(e) {
+    e.preventDefault()
+    tempInput = tempInput.value
+    if (document.getElementById("F").checked) {
+        newTemp = (tempInput - 32) * (5/9)
+    } else {
+        newTemp = (tempInput * 9/5 ) + 32
+    }
+    output.innerText = newTemp
+
+    console.log("newTemp")
+}
