2da6f871831e3ca3e3e275abe7d8e41d382e5974
palo89 <alison_lo89@yahoo.com>
2020-12-17 09:40:34 -0700

    added a few more notes and attempted to add more code

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ console.log('Hello, front end');
 document.addEventListener('DOMContentLoaded', () => {
 console.log("DOM LOADED")
 
-  // get our user submission from form
+  // get our user to enter temperture
   let submitButton = document.querySelector("input[type=submit]")
 
   submitButton.addEventListener("click", (e) => {
       e.preventDefault()
 
       let res = document.getElementById("submitText").value
+
+
+    // convert the temperature from F to C
