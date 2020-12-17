d279a2ef75746b5258836fec0721ff081dd54e26
palo89 <alison_lo89@yahoo.com>
2020-12-17 09:51:19 -0700

    missing closing tags in JavaScript, added.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 console.log('Hello, front end');
 document.addEventListener('DOMContentLoaded', () => {
 console.log("DOM LOADED")
+}
 
   // get our user to enter temperture
   let submitButton = document.querySelector("input[type=submit]")
 
   submitButton.addEventListener("click", (e) => {
       e.preventDefault()
+  }
 
       let res = document.getElementById("submitText").value
 
