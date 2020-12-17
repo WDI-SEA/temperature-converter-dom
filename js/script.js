203b5f4376170e14210292056f73e0ba787de391
ElyssaW <ejwinch1@gmail.com>
2020-12-16 20:47:00 -0600

    Initialized basic Javascript: Webpage accepts input and produces output

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('Hello, front end');
+//document.addEventListener('DOMContentLoaded', ()=>{
+//
+//    myDiv.addEventListener('click', (event) => {
+//        console.log(event);
+//        event.target.style.size = event.target.style.size + 10;
+//    })
+//    
+//     document.querySelector('form').addEventListener('submit', (e)=>{
+//         e.preventDefault()
+//         console.log(document.querySelector('#info').value)
+//     })
+//    
+//    let a = document.createElement('a')
+//    a.href ='http://hackertyper.com/'
+//    a.textContent = 'hack!'
+//    
+//    document.body.appendChild(a)
+//})
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('Hello, front end');
+    
+    document.querySelector('form').addEventListener('submit', (e)=> {
+        e.preventDefault();
+        console.log(document.querySelector('#temp').value)
+        
+        output.innerText = document.querySelector('#temp').value
+    })
+    
+})
