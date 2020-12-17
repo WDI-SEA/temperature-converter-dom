66c9b97fe591ebbe43e0219c7139daa00d598d4e
palo89 <alison_lo89@yahoo.com>
2020-12-16 21:03:46 -0700

    corrected some codes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', () => {
+console.log("DOM LOADED")
+
+  // get our user submission from form
+  let submitButton = document.querySelector("input[type=submit]")
+
+  submitButton.addEventListener("click", (e) => {
+      e.preventDefault()
+
+      let res = document.getElementById("submitText").value
