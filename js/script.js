8220624e506404c8bfc00f2463240297e66b5162
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2020-12-17 10:51:46 -0600

    Temperature Converter homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello, front end');
+document.querySelector("#submit").addEventListener("click", (e)=> convert(e))
+
+document.querySelector("#clear").addEventListener("click", convert)
+
+let output = document.getElementById("output")
+
+let temp = document.getElementById("temp")
+
+function convert(e) {
+    e.preventDefault()
+    temp=temp.value
+    let newTemp
+    if (document.getElementById("F").checked) {
+         newTemp=(temp-32)*.5556
+    } else {
+        newTemp = (temp*9/5+32)
+    }
+    output.innerText = newTemp
+    console.log("Hello")
+    
+}
+
+
+
+
