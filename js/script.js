24a613f0ea8447ffd23313e67d983bf9e41aca3e
JJDev <dancesfire@gmail.com>
2020-12-16 23:53:06 -0800

    solved for conversion output, step one!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('Hello, front end');
+document.querySelector("form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    document.querySelector("#practice").value
+    let temp =((practice.value-32)*.5556)
+    //console.log(document.querySelector("#practice").value + temp)
+    return document.querySelector(".result").innerHTML = temp 
+    
+    console.log(temp)
+    let cel = ((practice.value-32)*.5556)
+    console.log(e.target)
+    })
+   //let yourAnswer = prompt('Whats the Temperature in Ferhinhight?', 'Yes/No');
+
+// let cel = ((#practice.value-32)*.5556)
+// console.log(cel)
