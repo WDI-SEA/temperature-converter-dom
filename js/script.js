9d3143ae574f79655e430dd76a1e8614be42c2da
SpeTzCoding <yuritischenko@ymail.com>
2020-12-17 08:52:34 -0800

    Temperature Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello, front end');
+function convertFtoC(tempF) {
+    const tempC = (tempF - 32) * (5/9);
+    return tempC;
+}
+
+function convertCtoF(tempC) {
+    const tempF = (tempC * 1.8) + 32;
+    return tempC;
+}
+
+function myFunction() {
+    document.getElementById("Results").click(); // Click on the checkbox
+}
+
+
+document.getElementById("Results").addEventListener("click", someOtherFunction);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let submitButton = document.querySelector("input[type=submit]")
+        submitButton.addEventListener("click", (e) => {
+        e.preventDefault()
+    let res = document.getElementById("submitText").value
+        document.getElementById("feedback").innerText = res
+})
