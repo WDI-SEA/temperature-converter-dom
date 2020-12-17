88ac3afabbf39631ae78d48016aeb1ed97608852
austintip <austin.d.tipograph@gmail.com>
2020-12-16 23:14:42 -0500

    Austin Tipograph Temp Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        let temperature = document.getElementById("userInput").value
+        if (document.getElementById("fahrenheit").checked) {
+       document.getElementById("convertedTemp").innerText = (temperature - 32) * 5 / 9
+    } else {
+    document.getElementById("convertedTemp").innerText = (temperature * 9 / 5) + 32
+    }
+    
+ 
+ })
+})
