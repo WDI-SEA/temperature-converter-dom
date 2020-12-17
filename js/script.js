00386d3849cc9533919012e7481aa00aafabcffe
kys3923 <kys3923@gmail.com>
2020-12-17 11:19:30 -0500

    added message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', () => {
+// if convert button clicks
+    document.querySelector('form').addEventListener("click", (e) => {
+    e.preventDefault();
+    
+        document.getElementById('clickA').onclick = 
+        function conversionTemp() {
+            var givenTemp = document.getElementById('submitTemp').value;
+            if (document.getElementById('submitTempC').checked) {
+            document.getElementById('answerNum').innerText = ((givenTemp * 9/5) + 32).toFixed(3);    
+            } else {
+            document.getElementById('answerNum').innerText = ((givenTemp - 32) * (5/9)).toFixed(3);  
+            }
+        }   
+    })
+// if clear button clicks
+    document.getElementById('clickB').addEventListener('click', (e) => {
+        document.getElementById('submitTemp').value = "";
+    })
+
+})
+
+
+
+
+
