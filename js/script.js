c3625e8bc1644d22d9c51d74eeee3f89f7a81ee4
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2020-12-17 09:05:21 -0800

    updated html, java and css to make a temperature converter.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, front end');
+console.log('this is a test!');
+function restart(event){
+  location.reload();
+}
+function convertTemp(event){
+  event.preventDefault();
+  let userInput = document.getElementById('userTemp');
+  userInput = userInput.value;
+  let temp = 0
+  if (document.getElementById('cel').checked) {
+    temp = ((userInput * 9/5) + 32);
+    convertedTemp.innerHTML = temp;
+    console.log(temp);
+  } else if (document.getElementById('far').checked) {
+    temp = (userInput * (5/9) -32);
+    convertedTemp.innerHTML = temp;
+    console.log(temp);
+  } else {
+    alert('Select Fahrenheit or Celcius!')
+  }
+}
+let convertedTemp = document.getElementById('results');
+document.getElementById('converter').addEventListener('submit', convertTemp);
+document.getElementById('reset').addEventListener('reset', restart);
