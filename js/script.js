a41dafc6c7b09728db1d59aec6dca2b7911236a3
damatoaj <mr.arthurdamato@gmail.com>
2020-12-17 08:48:21 -0800

    fourth

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ clearButton.addEventListener("click," (e) =>)
 
 let solution = document.getElementById("solution")
 
+let celsius = document.querySelector("#C")
+celsius.addEventListener("click", (e) =>)
+
+let fahrenheit = document.Selector("#F")
+celcius.addEventListener("click", (e) =>)
+
+//Identify Variables
+var f == ((x - 32) * 5556) && #F
+var c == ((x * 1.8) + 32) && #C
+
+
