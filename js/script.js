9bb12b7990618c1367e75ad67be12bea37b44d95
sameghosts <seanfeiner.brown@gmail.com>
2020-12-18 00:50:58 -0500

    First attempts on tempconverter, only HMTL, CSS skeleton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("👋 Hello!")
+    
+    // get our user submission from our form
+    
+    let submitButton = document.querySelector("input[type=submit]")
+
+    
+    submitButton.addEventListener("click", (e) => {
+        e.preventDefault()
+    
+
+    let res = document.getElementById("submitText").value
+    let radio = document.getElementById("radio").value
+
+    //take user submited text and set as innerText to div id feedback
+    document.getElementById("rectangle").innerText = "The temperature is " + res + " " + radio + <br> + res
+
+
+
+})  
+
+
+})
+
+//get form reference
+//get the import for the temp
+// how to convert
+// check which raido button is clicked
+if F
+    
