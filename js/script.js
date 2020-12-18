974ca78fd2f1fe1b732425a4e9d6cf8a1c1625ce
sameghosts <seanfeiner.brown@gmail.com>
2020-12-18 01:34:16 -0500

    Cleaned up form HTML

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => 
     console.log("👋 Hello!")
+
     
     // get our user submission from our form
     
     let submitButton = document.querySelector("input[type=submit]")
 
     
-    submitButton.addEventListener("click", (e) => {
-        e.preventDefault()
+    //submitButton.addEventListener("click", (e) => {
+      //  e.preventDefault()
     
 
-    let res = document.getElementById("submitText").value
-    let radio = document.getElementById("radio").value
+    let reg = document.getElementById("submitText").value
+    let rad = document.getElementById("radio").value
 
     //take user submited text and set as innerText to div id feedback
-    document.getElementById("rectangle").innerText = "The temperature is " + res + " " + radio + <br> + res
-
-
-
-})  
-
-
-})
-
-//get form reference
-//get the import for the temp
-// how to convert
-// check which raido button is clicked
-if F
     
+    //document.getElementById("rectangle").innerText = '<p> The temperature is </p>  + reg  + rad '
