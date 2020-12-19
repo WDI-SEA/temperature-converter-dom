204441323b128d7553538a2add6c0a9dff366f4c
jaystar007 <jstarks0@Hotmail.com>
2020-12-18 17:48:19 -0800

    turning in HW

---
+++ Array.js
@@ -0,0 +1,27 @@
+var people = [
+	{
+		name: "Bob",
+		coolnessScore: 23
+	},
+	{
+		name: "Jen",
+		coolnessScore: 40
+	},
+	{
+		name: "James",
+		coolnessScore: 15.6
+	},
+	{
+		name: "Isolde",
+		coolnessScore: 17
+	}
+];
+
+// (1) Write code that uses the filter function, and the below isCool function to return a list of people who are cool.
+
+var isCool = function(person) {
+	return person.coolnessScore > 20;
+}
+
+// Your code goes here
+const coolPeople = people.filter(isCool)
--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    let res = document.getElementById("submitText").value
+
+    document.getElementById("feedback").innerText = res
+
+    /*Fahrenheit to Celsius Formula	
+(32°F − 32) × 5/9 = 0°C */
+
+/* Celsius to Fahrenheit
+(32°C × 9/5) + 32 = 89.6°F
+
+//---Steps for planning out code---/
+//get form reference. 
+//0Get the import for the Temp.
+//How to convert it.
+//Check which Radio button is clicked.
+//"if F then"
+    //convert the imported Temp. from C to F
+//else
+    //convert Temp. from F to C
+//Displayu results on the DOM
