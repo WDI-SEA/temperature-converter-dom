3ab7a3edf4cc26010f3f05429cf62bc565034316
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2020-12-20 20:48:26 -0500

    was not able to finish up the javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('DOM has been loaded!!!')
+    document.getElementById('submit').addEventListener('click', (e)=> convert(e))
+
+    // when clear is clicked it 
+    // document.querySelector('#clear') 
+
+    // made a variable for the top box where you put the number you want to convert
+    let tempInput = document.getElementById('tempInput')
+
+
+    // made a variable for the red box called 'finalResults
+    let finalResult = document.getElementById('finalDisplay')
+
+    function convert(e) {
+        e.preventDefault()
+        
+    }
+    // when submit is clicked it rins function convert
+
+
+})
+
