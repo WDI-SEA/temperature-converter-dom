830e48bb6ce7a6f89539447d0dcd4bdde9f25337
sameghosts <seanfeiner.brown@gmail.com>
2020-12-27 15:40:20 -0800

    changed form to selections added console logs for temp conversions

--- js/script.js
+++ js/script.js
@@ -1,36 +1,52 @@
-//set up event listener 
-//Get our user Submission of form
-
-
-
-
-     //document.addEventListener('DOMContentLoaded', () => {}
-       // console.log("👋 Hello!"))
-
-    
-    // get our user submission from our form
-    
-   // let submitButton = document.querySelector("input[type=submit]")
-
-    
-    //submitButton.addEventListener("click", (e) => {
-      //  e.preventDefault()
-    
-
-    // let reg = document.getElementById("submitText").value
-    // let rad = document.getElementById("radio").value
-
-    //take user submited text and set as innerText to div id feedback
-    
-    //document.getElementById("rectangle").innerText = '<p> The temperature is </p>  + reg  + rad '
-
+// Set the Form and load it 
+// have event listeners
 
     //get form data, load it 
-    var form = document.querySelector("form");
-    form.addEventListener("submit", function(event)){
-      console.log("Saving value", form.elements.value.value);
-    //   event.preventDefault();
-    }
+const form = document.querySelector("#myForm");
+const inputValue = document.getElementById("submitText");
+const tempUnit = document.getElementById("unitSelect");
+const rectangle = document.getElementById("rectangle");
+let tempF;
+let tempC;
+let tempK;
+let tempChangeValue;
+
+tempChangeValue
+
+
+//event listener for form submit
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  console.log(event);
+  // console.log(inputValue.value);
+  // console.log(tempUnit.value);   
+  
+  switch (tempUnit.value){
+    case 'fahrenheit':
+      console.log(`${inputValue.value} ${tempUnit.value}`);
+      tempF = inputValue.value;
+      tempC = Math.floor((5/9)*(tempF-32));
+      console.log(`${tempC} celcius`);
+      tempK = Math.floor(((5/9)*(tempF-32))+273);
+      console.log(`${tempK} kelvin`);
+      document
+      break
+      case 'celcius':
+      tempC = inputValue.value;
+      tempF = Math.floor((9/5)*(tempC+32));
+      console.log(`${tempF} fahrenheit`);
+      tempK = Math.floor((tempC+273));
+      console.log(`${tempK} kelvin`);
+      break
+      case 'kelvin':
+      tempK = inputValue.value;
+      tempF = Math.floor(((9/5)*(tempK-273))+32);
+      console.log(`${tempF} fahrenheit`);
+      tempC = Math.floor((tempK-273));
+      console.log(`${tempC} celcius`);
+      break
+  }
+})
 
     //   var sock = document.getElementsByName("tempUnit");
     //   function getData (event) {
