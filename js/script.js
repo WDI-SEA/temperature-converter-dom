bb5beb67ad994e961531a18dd6c170d6a1f33a84
SunilDeveloper7 <sunnil474@gmail.com>
2021-01-29 08:58:06 -0800

    JS Temp Converter,added html and few css and one function

--- js/script.js
+++ js/script.js
@@ -5,13 +5,12 @@ function tempConvert() {
     
     var fahrenheit = document.getElementById("fahrenheit").value;
     var celsius = document.getElementById("celsius").value;
-  
-  if (fahrenheit != '') {
+
+if (fahrenheit != '') {
         celsius = (parseFloat(fahrenheit) - 32) / 1.8;
     } else {
         fahrenheit = (parseFloat(celsius) * 1.8) + 32;
     }
-  
 
 
     document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
