cc45f8bf31fc6c027df99137191a4f43ab4bf8f8
jaackie25 <jackiedinh91@gmail.com>
2021-05-06 23:15:57 -0700

    added all changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello, front end');
+let tempF= document.getElementById("fahrenheit")
+let tempC= document.getElementById("celcius")
+let newTemp= document.getElementById("newValue")
+
+
+
+// let userInput = document.querySelector("#temp").value
+// console.log(userInput)
+
+function handleMath(userInput){
+    if(tempF.checked==true){
+        return (Math.floor((userInput-32) * 0.56) + "C")
+        
+    } else if (tempC.checked==true){
+        return (Math.floor((userInput * 1.8) + 32) + "F")
+       
+    }
+}
+
+document.querySelector("#submit").addEventListener("click", (e) => {
+    e.preventDefault()
+    let userInput = document.querySelector("#temp").value
+    let convertedValue= handleMath(userInput)
+    newTemp.innerText=convertedValue
+    
+})
+
+document.getElementById("clear").reset(myForm)
