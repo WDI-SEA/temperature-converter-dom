df3fd25a2bc299aba0cd6ce866a9d836321790da
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-07 09:55:22 -0500

    Added color-change function to temp. output

--- js/script.js
+++ js/script.js
@@ -1,18 +1,57 @@
+let CelSolution = null;
+let FarSolution = null;
+
 function getTemperature() {
     let inputTemp = document.getElementById("temperature").value
     if (document.getElementById("fahrenheit").checked == true) {
         let Far = inputTemp
-        let CelAnswer = Math.round((Far - 32) * 5 / 9)
-        document.getElementById("answer").innerText = CelAnswer + "°C"
+        CelSolution = Math.round((Far - 32) * 5 / 9)
+        document.getElementById("answer").innerText = CelSolution + "°C"
+        celColorChange()
     } else if (document.getElementById("celsius").checked == true) {
         let Cel = inputTemp
-        let CelAnswer = Math.round(Cel * 9 / 5 + 32)
-        document.getElementById("answer").innerText = CelAnswer + "°F"
+        FarSolution = Math.round(Cel * 9 / 5 + 32)
+        document.getElementById("answer").innerText = FarSolution + "°F"
+        farColorChange()
     }
 }
+
+// DEFECTIVE SOLUTION
+// function colorChange() {
+// if (CelSolution <= 0 || FarSolution <= 32) {
+//     document.getElementById("answer").style.backgroundColor = "blue"
+// } else if (CelSolution >= 32 || FarSolution >= 90) {
+//     document.getElementById("answer").style.backgroundColor = "crimson"
+// } else {
+//     document.getElementById("answer").style.backgroundColor = "white"
+// }
+// }
+
+function celColorChange() {
+    if (CelSolution <= 0) {
+        document.getElementById("answer").style.backgroundColor = "blue"
+    } else if (CelSolution >= 32) {
+        document.getElementById("answer").style.backgroundColor = "crimson"
+    } else {
+        document.getElementById("answer").style.backgroundColor = "white"
+    }
+}
+
+function farColorChange() {
+    if (FarSolution <= 32) {
+        document.getElementById("answer").style.backgroundColor = "blue"
+    } else if (FarSolution >= 90) {
+        document.getElementById("answer").style.backgroundColor = "crimson"
+    } else {
+        document.getElementById("answer").style.backgroundColor = "white"
+    }
+}
+
 
 function clearAnswer() {
     document.getElementById("temperature").value = " ";
     document.getElementById("answer").innerText = " ";
+    document.getElementById("answer").style.backgroundColor = "white"
 }
+
 
