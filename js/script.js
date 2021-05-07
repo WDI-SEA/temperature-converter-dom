952c2771fe3d4f100de1858171785437d7028a8c
SpiralV <MVelasco90@hotmail.com>
2021-05-07 09:01:55 -0700

    sort of complete idk

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello, front end');
+window.addEventListener("DOMContentLoaded", () => {
+    let io = document.getElementById("finresult")
+    if (.prenumber >= 70) {
+        io.style.backgroundColor = 
+    }
+
+    //F to C (valNum-32)/1.8
+    //C to F (valNum*1.8)+32
+
+    /* add listener for input,
+    use radio to choose which conversion,
+    output box display finresult/postnumber (unsure)
+    clear button to reset page
