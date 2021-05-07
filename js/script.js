5d0d9ed2f79ac10185921cfb9f3b29cbd7fb0be6
JamesT <arcaneonline1@gmail.com>
2021-05-06 22:22:09 -0700

    Solutions - Not Finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello, front end');
+"use strict";
+var $ = function(id) { return document.getElementById(id); };
+
+
+var clearTextBoxes = function() {
+    $("degrees_entered").value = "";
+    $("degrees_computed").value = "";
+}
