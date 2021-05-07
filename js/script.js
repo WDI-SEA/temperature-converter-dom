8febfb51d5c3dba8e91c94e78ea56aeb84ecc449
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-06 22:16:32 -0500

    Full solve (no bonus goals).

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello, front end');
+function getTemperature() {
+    let inputTemp = document.getElementById("temperature").value
+    if (document.getElementById("fahrenheit").checked == true) {
+        let Far = inputTemp
+        let CelAnswer = Math.round((Far - 32) * 5 / 9)
+        document.getElementById("answer").innerText = CelAnswer + "°C"
+    } else if (document.getElementById("celsius").checked == true) {
+        let Cel = inputTemp
+        let CelAnswer = Math.round(Cel * 9 / 5 + 32)
+        document.getElementById("answer").innerText = CelAnswer + "°F"
+    }
+}
+
+function clearAnswer() {
+    document.getElementById("temperature").value = " ";
+    document.getElementById("answer").innerText = " ";
+}
+
