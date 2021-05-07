e92d2b49cc1f3242067654f3295a114248a5b815
rei-bp <reinard.bp@gmail.com>
2021-05-07 01:05:47 -0700

    temperature converter 5/06 deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello, front end');
+
+function convert(input) {
+     if (document.getElementById("F").checked = true) {
+        return (input - 32) * .55        
+    } else (document.getElementById("C").checked = true); {
+        return (input % .55) + 32
+
+}
+}
+
+document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault()
+    let input = document.getElementById('input').value
+    let answer = convert (input)
+    let solution = document.getElementById('finalAnswer')
+    solution.innerText = answer
+})
+
