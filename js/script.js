8ecb2ee9ecdebc10759d3ed0a0b763225239a418
anaismveras <verasmanais@gmail.com>
2021-10-14 18:52:00 -0400

    added fucntion to convert F to C by pressing button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
-submitButton.addEventListener('click', () => {
-    let getTemp = document.getElementById('temp-input')
+function workingRadioButtons () {
+    const fToCRadioButton = document.getElementById('fahrenheitButton')
+    // const cToFRadioButton = document.getElementById('CelciusButton') 
+        if (fToCRadioButton == true) {
+            submitButton.addEventListener('click', () => {
+                // wiriting whats in the form when the button is pressed
+                let getTemp = document.getElementById('temp-input').value;
+                //printing the convertion when the button is pressed
+                let celciusEquation = Math.floor((((getTemp - 32) *5) /9))
+                document.getElementById('temp-in-c').innerHTML = celciusEquation
+            })
+        } ;
+        };
 
-}) 
+
+
