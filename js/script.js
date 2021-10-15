c0aa8166f54c6a4550483a3a2c461636a75d4d89
steven43533 <steven43533@gmail.com>
2021-10-14 23:59:16 -0400

    Requirements for deliverable complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-console.log('Hello, front end');
+//function to clear the text inside the textbox
+function clearText() {
+
+    document.getElementById("temp").value = "";
+}
+
+const radioButtonSelection = () => {
+    const fahrenheitButton = document.getElementById("fheit")
+    const celsiusButton = document.getElementById("celsi")
+
+   
+    if(fahrenheitButton.checked){
+       let a = convertCelsius()
+       document.getElementById("rectangle").innerHTML = a + '°C';
+    } else if(celsiusButton.checked) {
+        let b = convertFahrenheit()
+        document.getElementById("rectangle").innerHTML = b + '°F';
+    } else {
+        console.log('No button selected.')
+    } 
+    
+}
+
+//function that converts the input from the textbox to celsius
+const convertCelsius = () => {
+    let fahrenheit = parseFloat(document.querySelector("#temp").value);
+    let celsiusResult = Math.floor((fahrenheit-32)*5/9);
+    return celsiusResult
+    // console.log('Temp in celsius is ' + celsiusResult)
+}
+//function that converts the input from the textbox to fahrenheit
+const convertFahrenheit = () => {
+    let celsius = parseFloat(document.querySelector("#temp").value);
+    let fahrenheitResult = Math.floor((celsius*9/5)+32)
+    return fahrenheitResult
+    // console.log('Temp in fahrenheit is ' + fahrenheitResult)
+}
+
+//document.getElementById("buttons").addEventListener('click', radioButtonSelection)
+
+// const submitListender = () => {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
