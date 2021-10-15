9ffdd6975a7c8906882d2fa3a6408405f2fbaf2f
anaismveras <verasmanais@gmail.com>
2021-10-14 21:23:23 -0400

    added functions for F and C Radio Buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+const getTemp = document.getElementById('temp-input').value;
+
+const fToCRadioButton = document.getElementById('fahrenheitButton')
+const cToFRadioButton = document.getElementById('CelciusButton')
 
 function workingRadioButtons () {
-    const fToCRadioButton = document.getElementById('fahrenheitButton')
-    // const cToFRadioButton = document.getElementById('CelciusButton') 
-        if (fToCRadioButton == true) {
-            submitButton.addEventListener('click', () => {
-                // wiriting whats in the form when the button is pressed
-                let getTemp = document.getElementById('temp-input').value;
-                //printing the convertion when the button is pressed
-                let celciusEquation = Math.floor((((getTemp - 32) *5) /9))
-                document.getElementById('temp-in-c').innerHTML = celciusEquation
-            })
-        } ;
-        };
+        if (cToFRadioButton.checked) {
+            displayCelsiusTemp()
+        } else if (fToCRadioButton.checked) {
+            displayfahrenheitTemp()
+        } }
 
+function displayCelsiusTemp () {
+    let toCelciusEquation = Math.floor(getTemp * 9/5 + 32)
+    document.getElementById('temp-in-h1').innerHTML = toCelciusEquation
+}
 
+function displayfahrenheitTemp() {
+    let toFahrenheitEquation = Math.floor(getTemp - 32 *5 /9)
+    document.getElementById('temp-in-h1').innerHTML = toFahrenheitEquation
+}
 
+submitButton.addEventListener('click', workingRadioButtons)
