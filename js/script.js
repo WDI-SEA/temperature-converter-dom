8b6186ebc4411c2ac908de9d1b96f3c7b55a181d
iwaggoner <iwaggoner95@gmail.com>
2021-10-14 22:59:47 -0400

    Updated style.css, index.html, script.js and added some photos and added some style based on temp

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ const submitButton = document.getElementById('sub_button')
 const resetButton = document.getElementById('re_button')
 const celsiusButton = document.getElementById('TempC')
 const kelvinsButton = document.getElementById('TempK')
+const container = document.createElement('div')
 const theAnswer = document.createElement('div')
 const newTemp = document.createElement('p')
+newTemp.id = 'newTemp'
+theAnswer.id = 'answer'
+container.className = 'container'
 
 const changeTemp = 'blank'
 
@@ -16,45 +20,58 @@ submitButton.addEventListener('click', () => {
     const x = document.getElementById('TempF').value
     const y = Math.round((x-32) * (5/9))
 
-	newTemp.id = 'newTemp'
-    theAnswer.id = 'answer'
+	  // newTemp.id = 'newTemp'
+    // theAnswer.id = 'answer'
     newTemp.textContent = y + ' C°'
 
     theAnswer.appendChild(newTemp)
+    container.appendChild(theAnswer)
 
-    document.body.appendChild(theAnswer)
+    document.body.appendChild(container)
   }
   else if (true == document.getElementById('TempK').checked){
     const x = document.getElementById('TempF').value
     const y = Math.round((x-32) * (5/9) + 273.15)
 
-	newTemp.id = 'newTemp'
-    theAnswer.id = 'answer'
+	  // newTemp.id = 'newTemp'
+    // theAnswer.id = 'answer'
     newTemp.textContent = y + ' K°'
 
     theAnswer.appendChild(newTemp)
 
-    document.body.appendChild(theAnswer)
+    container.appendChild(theAnswer)
+
+    document.body.appendChild(container)
   }
   else {
 
-	newTemp.id = 'newTemp'
-    theAnswer.id = 'answer'
+	  // newTemp.id = 'newTemp'
+    // theAnswer.id = 'answer'
     newTemp.textContent = 'Oops, please select a Temperature to change too!'
 
     theAnswer.appendChild(newTemp)
 
-    document.body.appendChild(theAnswer)
-  }
+    container.appendChild(theAnswer)
 
-//   if(32 > document.getElementById('TempF').value) {
-//     document.getElementById('answer').style.backgroundImage = url('css/NiceDay.jpg')
-//   }
+    document.body.appendChild(container)
+  }
+    const x = document.getElementById('TempF').value
+    
+   if(32 > x) {
+    document.getElementById('answer').style.backgroundImage = "url('js/ColdDay.jpg')"
+    console.log(x)
+   }
+   else if (80 < x) {
+    document.getElementById('answer').style.backgroundImage = "url('js/HotDay.jpg')"
+   }
+   else {
+    document.getElementById('answer').style.backgroundImage = "url('js/NiceDay.jpg')" 
+   }
 
 })
 
 resetButton.addEventListener('click', () => {
-  document.body.removeChild(theAnswer) 
+  document.body.removeChild(container) 
   document.getElementById('TempC').checked = false
   document.getElementById('TempK').checked = false
   document.getElementById('TempF').value = ''
