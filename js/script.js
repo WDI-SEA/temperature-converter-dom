4ac4e99c37dc8787da6c95521a1d6a50d7b6d974
galyverasi <galyverasi@gmail.com>
2021-10-15 06:51:51 -0600

    deliverable-dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+//two functions that will convert celcius to fahrenheit and vice versa
+const celciusToFahrenheit = (inputTemp) => {
+    return inputTemp * 9 / 5 + 32
+}
+
+const fahrenheitToCelcius = (inputTemp) => {
+    return (inputTemp - 32) * 5 / 9
+}
+// detect whether the button was clicked
+document.getElementById('submit').addEventListener('click', () => {
+    console.log('submit button clicked')
+// take the value from the input field
+    const inputTemp = document.getElementById('input-temp').value
+    console.log('temp entered', inputTemp)
+
+
+
+
+
+
+
+
+
+
+
+    
