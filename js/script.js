3985da03d42bce07d47701a6ca8b7f8a34b38736
eddycodes27 <eddycodes27@gmail.com>
2021-10-15 00:53:20 -0400

    Temperature Converter HW

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+function convertTemp() {
+    let celsius = document.getElementById("Cvalue")
+    let fahrenheit = document.getElementById("Fvalue")
+
+    if (celsius.value != "") {
+        fahrenheit.value = Math.round(celsius.value * 9 / 5 + 32)
+        celsius.value = ""
+    } else { 
+        celsius.value = Math.round((fahrenheit.value - 32) * 5 / 9)
+        fahrenheit.value = ""
+    }
+}
