7b06a795a4ff6ab95ee9b445c0af3f2045f46e2f
Jmeier2715 <YOUR-EMAIL-ADDRESS>
2021-10-15 00:13:32 -0400

    Jons first solution without much css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+//this looks so bad and is definitely not the "correct" answer
+
+document.getElementById("convert").onclick = tempConvert;
+document.getElementById("clear").onclick = resetBox;
+function tempConvert(){
+    var fTemp = document.getElementById('fahrenheit').value;
+    if (fTemp!=''){
+       alert((fTemp-32)*(5/9))}
+    }
+function resetBox(){
+    document.getElementById("fahrenheit").value = '';
+}
