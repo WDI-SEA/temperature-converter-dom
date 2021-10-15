f3d1c1c2579a4aba3f9b524623ef7f55e1e9800c
cmiltiadou <cmiltiad@gmail.com>
2021-10-14 23:35:58 -0400

    not fully finished, need to fix input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-console.log('Hello, front end');
+
+// // event listening function to convert when submit is pressed
+// //make function to convert from celsius to fahrenheit 
+
+const capturedTemp = document.getElementById("tempInput").value 
+
+const fToCelsius = (capturedTemp - 32 * 5/9)
+
+const CelsiusToF = (capturedTemp + 32 * 9/5)
+
+function choseFahr () {
+    return true ;
+}
+
+function choseCels () {
+    return true ;
+}
+
+function clearTemp() {
+    document.getElementById('tempInput').value = "" ;
+}
+
+function convertTemp () {
+    // console.log('this works');
+    // if (choseFahr === true) {
+    console.log(capturedTemp)
+       
+    }
+   
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('submit').addEventListener('click', convertTemp)
+document.getElementById('fahr').addEventListener('click', choseFahr)
+document.getElementById('cels').addEventListener('click', choseCels)
+
+
