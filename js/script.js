4cbe13fe14620f46330137d6f0172e47f8ae6a09
shuzel99 <shuzellide@gmail.com>
2021-10-14 23:54:04 -0400

    tempConvert

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+}
+
+
+// let F = 76
+// let C = (F -= 32)/1.8
+// if  (F = 76){
+//   console.log(F + " degrees Fahrenheit is " + C + " degrees Celsius!")
+// }
+
