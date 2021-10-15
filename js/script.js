3fb28a18dbccd5d390218e8d1e611f8d14ef82fe
DagemBefikadu <befikadu.dagem@gmail.com>
2021-10-15 06:09:27 -0700

    Homework 3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, front end');
+const newTempC = document.getElementById('tempC')
+const newTempF = document.getElementById('tempF')
+
+function userTemp() {
+  console.log('it works')
+
+  const newInput = document.getElementById('temperature').value
+  if (newTempC.checked) {
+    let convertCtoF = Math.floor((newInput*1.8)+32)
+    document.getElementById('tempFinal').value= convertCtoF + "°F"
+  } else if(newTempF.checked ) {
+  let convertFtoC = Math.floor(((newInput-32)*5)/9)
+  document.getElementById('tempFinal').value= convertFtoC + "°C";
+  } 
+}
+
+
+function clearTemp() {
+  console.log('bye felica')
+  document.getElementById('temperature').value = ''
+  document.getElementById('tempFinal').value = ''
+  document.getElementById('tempF').checked = false
+ document.getElementById('tempC').checked = false
+}
