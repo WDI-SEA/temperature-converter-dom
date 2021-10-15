dbfc88b8918a0a5ae2b81c0afdf1f60b67fd00c6
DG-98 <dgarconpro@gmail.com>
2021-10-14 23:44:57 -0400

    temp has been converted

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+document.getElementById('submit-temp').addEventListener("click", submitTemp);
+document.getElementById('clear-temp').addEventListener("click", clearTemp);
+
+
+function clearTemp() {
+  document.getElementById("Temp").innerHTML = " ";
+}
+
+function submitTemp() { 
+  if(document.getElementById('f').checked) { convertCtF(); } else if(document.getElementById('c').checked) { convertFtC(); } 
+}
+
+
+function convertFtC() {
+ const farTemp = document.getElementById("text-box").value;
+  
+  document.getElementById("Temp").innerHTML = Math.floor((farTemp - 32)*5/9);
+ 
+}
+
+function convertCtF() {
+  const celcTemp = document.getElementById("text-box").value;
+  
+  document.getElementById("Temp").innerHTML = Math.floor((celcTemp * 9/5)+32);
+}
+
