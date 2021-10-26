79b69490487ada9094d3d3f7695f8afcf78ba764
hannahlegros <hannahrlegros@gmail.com>
2021-10-25 23:15:56 -0400

    added html, some js and some CSS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-document.getElementById('calculate')
+const tempInput = document.querySelector('#temp').value
+const submit = document.querySelector('#submit')
+const clear = document.querySelector('#clear')
+const fahrenheit = document.querySelector('#fahr')
+const celsius = document.querySelector('#cels')
+const answerDiv = document.querySelector('#answer')
 
+const celsToFahr = (temp) => {
+    (temp-32)/1.8
+}
+const fahrToCels = (temp) => {
+    (temp*2)+30
+}
 
-let fahrenheit = ()
-let celsius = (fahrenheit - 32) * 5/9
-
-
-
+const convertTemp = (e) => {
+    e.preventDefault()
+    if (fahrenheit.checked === true){
+        fahrToCels(tempInput)
+        console.log('this is the output:\n', convertTemp())
+    }
+    else if (celsius.checked === true){
+        celsToFahr(tempInput)
+    }
+}
+console.log(tempInput.value)
+submit.addEventListener('submit', convertTemp)
