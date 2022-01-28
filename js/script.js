863619ffe258b86c0d809492849ef7a78e6ea752
vijaylpersad <vijaylpersad@utexas.edu>
2022-01-28 11:32:09 -0600

    temperature-converter-dom

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ console.log('Hello, front end');
 
 function temperatureConverter(valNum) {
     valNum = parseFloat(valNum);
-    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+    document.getElementById("outputCelsius").innerText = (valNum-32) / 1.8;
 }
 
 function temperatureConverterTwo(valNum) {
     valNum = parseFloat(valNum);
-    document.getElementById("outputFahrenheit").innerHTML = ((valNum*1.8) + 32);
+    document.getElementById("outputFahrenheit").innerText = ((valNum*1.8) + 32);
 }
+
+/*solution:
+document.querySelector('#submit-button').addEventListener('click', function() {
+    console.log('caluculate the temp')
+    const tempInput = document.querySelector('#temp-input')
+    const result = document.querySelector('#result')
+
+    result.innerText = (tempInputValue - 32) * (5/9)
+})
