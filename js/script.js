07f1d9a5a9fef94978541c06ccf214114ece0b25
akhoagland <akhoagland@gmail.com>
2022-01-28 12:21:24 -0500

    commit message here

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello, front end');
+//console.log('Hello, front end');
+
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById(
+    clear).onclick = clearForm;
+
+
+
+function tempConvert(){
+    var fahrenheight = document.getElementById("fahrenheight").value;
+    var celcius = document.getElementById("celcius").value;
+
+    if(fahrenheight != '')
+    {
+        celcius =
+        (parseFloat(fahrenheight)-32)/1.8;
+        } else {
+            fahrenheight = (parseFloat(celcius)*1.8) + 32;
+            
+        }
+
+        document.getElementById('fahrenheight').value = parseFloat(fahrenheight).toFixed(1);
+
+        document.getElementById('celcius').value = parseFloat(celcius).toFixed(1);
+    }
+
+    function clearForm() {
+
+        document.getElementById('fahrenheight').value = '';
+
+        document.getElementById('celcius').value = '';
+    }
