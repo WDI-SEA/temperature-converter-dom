a42ef19c8d507f3541a2b8cf1d8bdaa0959c92b2
evankski <evankarwowski@gmail.com>
2022-01-28 09:39:49 -0800

    Finishes the temp deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, front end');
+document.querySelector('#submit-button').addEventListener('click', function(){
+    // console.log('button test')
+    const tempInputValue = document.querySelector('#temp-input').valueAsNumber
+    //creates an input var that is linked to inputID in html
+    const result = document.querySelector('#result')
+
+    result.innerText = (tempInputValue - 32) * (5/9)
+})
+
+
+
+// function getInput() {
+//     var x = document.getElementById("degreesFahrenheit").value;
+//     document.getElementById
+// }
+
+// let tempFahrenheight = 90
+
+// let tempCelsius = (tempFahrenheight - 32) / 1.8
+// console.log(tempFahrenheight + " degrees Farenheit is " + tempCelsius + " degrees celsius")
+
+
+
+
+
+// let testing = 'test'
+// document.getElementById('results').innerText = testing
