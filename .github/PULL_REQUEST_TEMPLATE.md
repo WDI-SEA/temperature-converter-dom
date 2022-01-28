ae3f5623c04720e1d109e3ba30b2e7380351ea68
Professorsurf <guevara.a.gabriel@gmail.com>
2022-01-28 09:50:22 -0800

    deliverable 1-27

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,8 +2,10 @@
 
 Please describe your comfort and completeness levels before submitting.
 
-Comfort Level (1-5): 
+Comfort Level (1-5): 3
 
-Completeness Level (1-5):
+Completeness Level (1-5):  3
 
 What did you think of this deliverable?:
+
+I could not figure out how to make the radio button have an effect on the output.  I feel like I am getting the concepts and when it comes to executing I am fallin way short. If only we could have more hours in a day :/
--- js/script.js
+++ js/script.js
@@ -1,1 +1,71 @@
-console.log('Hello, front end');
+// document.querySelector('#submit-button').addEventListener('click', function() {
+//     console.log('calculate the temp')
+//     const tempInput = document.querySelector('#temp-input').valueAsNumber
+//     console.dir(tempInput.ValueAsNumber)
+//     const result = document.querySelector('result')
+
+//     result.innerText = (tempInputValue - 32) * (5/9)
+
+
+// })
+
+
+document.querySelector('temp-form').addEventListener('submit', function(event) {
+event.preventDefault()
+console.log('the form is submitted')
+// console.log('calculate the temp')
+// const tempInput = document.querySelector('#tempInput').valueAsNumber
+// console.dir(tempInput.ValueAsNumber)
+// const result = document.querySelector('result')
+
+tempInpput.value = 72
+console.log(tempInput)
+})
+
+
+
+
+// let celcius;
+// let fahrenheit;
+
+// const celToFahr = (c) => {
+// celcius = c;
+// fahrenheit = c * 1.8 + 32;
+// console.log(`${celcius} C is ${fahrenheit} F`);
+// };
+
+// const fahrToCel = (f) => {
+// fahrenheit = f;
+// celcius = (f - 32) / 1.8;
+// console.log(`${fahrenheit} F is ${celcius} C`);
+// };
+
+// celToFahr(10);
+// fahrToCel(50);
+
+
+
+
+// OR
+
+// console.log('Hello, front end');
+
+// function cToF(celsius) 
+// {
+//   let cTemp = celsius;
+//   let cToFahr = cTemp * 9 / 5 + 32;
+//   let message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+//     console.log(message);
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   let fTemp = fahrenheit;
+//   let fToCel = (fTemp - 32) * 5 / 9;
+//   let message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//     console.log(message);
+// } 
+// cToF(80);
+
+// fToC(45);
+
