03644ccf2f63572fea8704174573f985e4fcb3ac
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-01-28 09:30:24 -0800

    converting temp deliverable finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello, front end');
+document.querySelector("#submit-button").addEventListener('click', function() {
+    // console.log("the button was clicked")
+    const tempInputValue = document.querySelector('#temp-input').valueAsNumber
+    const result = document.querySelector('#result')
+
+    result.innerText = (tempInputValue - 32) * (5/9)
+
+})
