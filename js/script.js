1ff27383882b8a70d1e643e75c0a77c50c0c8872
br1anlee <joonmolee3@gmail.com>
2022-01-28 09:03:11 -0800

    did not finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('Hello, front end');
+// define variables
+const convertF = document.querySelector("#fahrenheit")
+const convertC = document.querySelector("celsius")
+const tempConvertInput = document.querySelector("input[type='text']")
+const ans = document.getElementbyId("#convertANS")
+
+
+
+
+//event listener for submit
+document.getElementById("submit").addEventListener("click", () => {
+
+
+
+
+let ans = document.getElementbyId("#convertANS")
+    if (convertF) {
+        const convertedF = (fahtoConvert - 32) * 5/9
+        ans.innerText = convertedF
+    } else if (convertC) {
+        const convertedC = (fahtoConvert * 1.8) + 32
+        ans.innerText = convertedC
+}
+
+
+// clear button
+document.querySelector("#clear").addEventListener("click", () => {
+    tempConvertInput.value = ""
+    })
+
+})
