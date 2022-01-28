b6e22c593bc275a318a8031c83982ea8b4707b6b
aimeemisaki <aimeejmisaki@gmail.com>
2022-01-28 09:41:39 -0800

    finishes temp-converter-dom deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello, front end');
+document.querySelector('#submit-button').addEventListener('click', function() {
+    const tempInputValue = document.querySelector('#temp-input').valueAsNumber
+    const result = document.querySelector('#result')
+
+    result.innerText = (tempInputValue - 32) * (5/9) 
+
+})
