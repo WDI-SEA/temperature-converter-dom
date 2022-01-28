1f6896565b02047ccccd65f0d79661560101c2f0
vijaylpersad <vijaylpersad@utexas.edu>
2022-01-28 10:49:38 -0600

    temperature-converter-dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('Hello, front end');
+
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+}
+
+function temperatureConverterTwo(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML = ((valNum*1.8) + 32);
+}
