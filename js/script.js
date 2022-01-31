2b31f041ef071116f690c06e4f14c1a47e76faea
“ikarabag1” <irmakkarabag@gmail.com>
2022-01-31 21:06:00 +0300

    almost there, go through it.txt file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello, front end');
+document.querySelector('#userTemp').addEventListener('click', function() {
+const userInput = document.getElementById('#userTemp').valueAsNumber
+const result = document.querySelector('#result')
+
+result.innerText = (userInput - 32) * (5/9)
+})
