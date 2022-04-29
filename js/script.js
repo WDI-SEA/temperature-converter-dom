bf9c2c643ad3ca8f7895d7bc88e2b9d688f331ab
“emilykiss” <emilylanekiss@gmail.com>
2022-04-29 10:15:12 -0700

    emilys deliverable4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('Hello, front end');
+
+function convertFtoC(Farenheit) {
+    return (Farenheit - 32) * .5556
+}
+
+
+document.querySelector("#input-button").addEventListener("click", function(){
+    const userInput = document.querySelector("#user-input")
+ const result = convertFtoC(userInput.value)
+    const resultLabel = document.querySelector("#result")
+    resultLabel.innerHTML = "The temperature is " + result + " degrees!"
+})
