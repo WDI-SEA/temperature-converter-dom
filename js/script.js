323c79893c62d40fc2a5fdde55beed4b5a4decd1
GianlorenzoDercole <johndercole01@gmail.com>
2022-04-28 21:26:06 -0700

    f to c homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('Hello, front end');
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.querySelector('#input-button').addEventListener('click', function () {
+        const userInput = document.querySelector('#user-input')
+
+        const inputSpan = document.querySelector('#input-span')
+       // console.log('button clicked')
+        //console.log(userInput.value)
+
+        inputSpan.innerText = Math.floor((userInput.value -32) * (5/9))
+    })
+
+
+
+
+
+
+
+    document.querySelector('#clear').addEventListener('click', function (){
+        const inputSpan = document.querySelector('#input-span')
+        inputSpan.innerText = ''
+
+    })
+
+
+})
