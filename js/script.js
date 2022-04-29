78d4d2771ef9b3714cf2bb534c63669fc41fa631
“erhaneth” <gumusucb21@gmail.com>
2022-04-29 13:04:01 -0700

    Temperature Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello, front end');
+const body = document.querySelector('body')
+  body.style.backgroundColor = 'orange'
+  body.style.textAlign = "center"
+  input
+
+  console.log(body)
+
+  document.getElementBySelector("write to convert").style.textAlign = "center"; 
+
+  document.addEventListener("")
