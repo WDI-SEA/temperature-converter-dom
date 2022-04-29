9877d6345094bff2b4689da620f55e9558b6af77
marzo20 <myjune91@gmail.com>
2022-04-28 22:33:03 -0700

    deliverable temperature converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log('Hello, front end');
+// const far = 
+// function calculateToCel(event){
+//     userInput − 32) × 5/9
+// }
+// function calculateToFar(){
+//     userInput × 9/5) + 3
+// }
+document.addEventListener('DOMContentLoaded',function(){
+    const userInput = document.querySelector('#user-input')
+    const radioButtons = document.querySelectorAll('input[name="temperature"]')
+    document.querySelector('#submit').addEventListener('click', function(){
+        const inputSpan = document.querySelector('#inputSpan')
+        // inputSpan.innerText = `User input is ${userInput.value}! I don't know how to do the radio button`   
+            
+            let calculationForC = (userInput.value - 32) * (5/9)
+            let calculationForF = userInput.value*(9/5) + 32
+            for (const radioButton of radioButtons) {
+                if (radioButton.checked) {
+                    // selectedTemp = radioButton.value;
+                    inputSpan.innerText = `${calculationForF} °F`
+                
+
+       }else{
+        inputSpan.innerText = `${calculationForC} °C`
+       } console.log(radioButton)
+       
+       
+    }
+})
+    document.querySelector('.clear').addEventListener('click', function(){
+        userInput.value = ''
+    })
+})
