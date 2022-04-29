4b40ef3895727f9c3403831f52138cc18a6dcd70
seiplet93 <seiplet93@gmail.com>
2022-04-29 13:15:00 -0400

    complete deliverable, temp converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello, front end');
+let tempInputVar = document.getElementById("tempInput").value;
+let tempOutputVar = document.getElementById("tempOutput").innerText;
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+  let tempInputVar = document.getElementById("tempInput").value;
+  if (document.getElementById("farenBtn").checked) {
+    tempInputVar = ((tempInputVar - 32) * 5) / 9;
+  } else if (document.getElementById("celBtn").checked) {
+    tempInputVar = (tempInputVar * 9) / 5 + 32;
+  }
+
+  document.getElementById("tempOutput").innerText = tempInputVar;
+});
+
+document.getElementById("clearBtn").addEventListener("click", function () {
+  document.getElementById("farenBtn").checked = false;
+  document.getElementById("celBtn").checked = false;
+  document.getElementById("tempInput").value = "";
+  document.getElementById("tempOutput").innerText = "Your temp here";
+});
