5f3b0eed95c3737ad4a647cd359bfd957e074140
GianlorenzoDercole <johndercole01@gmail.com>
2022-07-31 14:40:03 -0700

    test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 console.log('Hello, front end');
+// let temp = 0
+// let unit = "farenhit"
+
+// const changeTempUnit = function(temp, unit) {
+//     if (unit === "farenhit"){
+//         let conversion = (temp -32) * (5/9)
+//         return conversion
+//     }
+//     else if (unit === "celcius") {
+//         let conversion = (temp - 32) * (5/9)
+//         return conversion
+//     }
+// }
 
 document.addEventListener('DOMContentLoaded', function(){
 
@@ -6,9 +19,13 @@ document.addEventListener('DOMContentLoaded', function(){
         const userInput = document.querySelector('#user-input')
 
         const inputSpan = document.querySelector('#input-span')
-       // console.log('button clicked')
-        //console.log(userInput.value)
+        // answer = changeTempUnit(unit, userInput)
+        // document.querySelector("#degrees-f").addEventListener("click", function(event){
+        //      unit = event.target.value
+        // })
+        // document.querySelector("#degrees-c").addEventListener("click", function(){
 
+        // })
         inputSpan.innerText = Math.floor((userInput.value -32) * (5/9))
     })
 
