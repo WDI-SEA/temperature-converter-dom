68086cb2e7c58978a57fc79f35f5e7a4138d863b
aimeepham14 <aimee.pham14@gmail.com>
2022-08-12 00:35:49 -0700

    temperature solution

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('Hello, front end');
+
+const numInputValue = document.getElementById("number");
+const submitBtn = document.getElementById("submit");
+const clear = document.getElementById("clear");
+const resetBtn = document.getElementById("reset");
+
+const temperatureConverter = () => {
+    let userInput = numInputValue.value;
+    let celsius = Math.floor((userInput - 32) * (5/9));
+  
+
+    const celsiusResult = `<p>${celsius} degrees Celsius </p>`;
+
+    if (userInput) {
+        result.innerHTML = celsius + ' degrees Celsius' 
+    }
+};
+
+submitBtn.addEventListener("click", temperatureConverter);
+
+numInputValue.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        temperatureConverter(e);
+    }
+}); 
+
+
+
+
+
+
