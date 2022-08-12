5e10b85da8b1b4bc09476702c86d60194175fc32
brandon-w1205 <bwu1205@gmail.com>
2022-08-11 17:19:39 -0700

    adds title, output display, input field, submit, and clear with basic functionality to output f to c

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+// assigns input field to a variable
+// let input = document.querySelector('#tempInp');
+
+// assigns Submit button to a variable
+// let buttonSubmit = document.querySelector('#Submit');
+
+// assigns output display to a variable
+let display = document.querySelector('.output .display')
+
+// on Submit click, takes input field variable, does F to C calculation and assigns C label
+Submit.addEventListener('click', function(e) {
+    display.innerHTML = Math.round((tempInp.value - 32) * (5/9));
+    display.innerHTML += " ℃";
+})
+
+// on Clear click, clears input field and output display
+Clear.addEventListener('click', function(e) {
+    display.innerHTML = "";
+    tempInp.value = "";
+})
+
+
+
