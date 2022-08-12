a6669ac32aecbb22f904326d416495e82c5cd34c
2manycooks <73556731+2manycooks@users.noreply.github.com>
2022-08-11 17:04:30 -0700

    mvp done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, front end');
+// variable declarations to grab everything
+
+let input = document.querySelector('#input')
+let fToK = document.querySelector('#fToK')
+let kToF = document.querySelector('#kToF')
+let clear = document.querySelector('#clear')
+let result = document.querySelector('#result')
+
+fToK.addEventListener("click", function(e) {
+    let preConversion = input.value
+    let postConversion = (preConversion - 32) * (5/9)
+    result.innerText = postConversion
+})
+
+kToF.addEventListener("click", function(e) {
+    let preConversion = input.value
+    let postConversion = (preConversion * 1.8) + 32
+    result.innerText = postConversion
+})
+
+clear.addEventListener("click", function(e) {
+    result.innerText = ''
+    input.value = ''
+})
