ed8ecec9cbe6bf586b1065e8c1d6141035ce9ebe
2manycooks <73556731+2manycooks@users.noreply.github.com>
2022-08-12 09:52:19 -0700

    added excellent gifs with if else statements for temp

--- js/script.js
+++ js/script.js
@@ -12,15 +12,29 @@ fToC.addEventListener("click", function(e) {
     let preConversion = input.value
     let postConversion = (preConversion - 32) * (5/9)
     result.innerText = postConversion
+
+    if (postConversion <= 0) {
+        document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+    } else if (postConversion > 38) {
+        document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+    }
 })
 
 cToF.addEventListener("click", function(e) {
     let preConversion = input.value
     let postConversion = (preConversion * 1.8) + 32
     result.innerText = postConversion
+
+    if (postConversion <= 32) {
+        document.body.style.backgroundImage = "url('https://media.giphy.com/media/xTeV7qibUsOYA3mmYw/giphy.gif')";
+    } else if (postConversion > 100) {
+        document.body.style.backgroundImage = "url('https://media.giphy.com/media/P7JmDW7IkB7TW/giphy.gif')";
+    }
 })
 
 clear.addEventListener("click", function(e) {
     result.innerText = ''
     input.value = ''
+    document.body.style.backgroundImage = ''
 })
+
