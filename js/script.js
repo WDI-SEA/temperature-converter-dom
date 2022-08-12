2d1fc8a059b917543e5e86d4abd121801fd3a575
Moodybleu <meganbenn27@icloud.com>
2022-08-11 18:52:49 -0700

    finishes to best of ability

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 console.log('Hello, front end');
 
-function temperatureConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+let theSquares = document.getElementsByClassName('square')
+console.log(theSquares[0])
+
+function tempConvert(temp, convert) {
+  if(convert.toLowerCase() === "c"){ 
+      temp = (temp - 32) * (5/9);
+      Math.floor()
+  } else if (convert.toLowerCase() === "k") {
+      temp = (temp + 459.67) * (5/9);
   }
+  return temp;
+}
+
+console.log(tempConvert(67, "c"));
+
+btn.addEventListener("click", function(e) {
+  console.log(e)
+})
+
+btn.addEventListener("click", function(e) {
+  console.log(input.value)
+})
