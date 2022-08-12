93273229578d897c3dd41945bbf3c2d1f5fdd06f
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-08-12 09:25:53 -0500

    James Onwordi's Deliverable for 12Aug ,2022

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log('Hello, front end');
+const textSection  = document.getElementById('textSec')
+
+const firstRadio = document.getElementById('radio1')
+const secondRadio = document.getElementById('radio2')
+const firstButton = document.getElementById('button1')
+const secondButton = document.getElementById('button2')
+let valueOfTemp
+let h1Value=document.createElement("h1");
+
+const div = document.getElementById("result")
+
+firstButton.addEventListener("click",function(){
+    appendValue(getTempInCelcius(textSection))
+})
+secondButton.addEventListener("click",function(){
+    h1Value.innerHTML=""
+    textSection.value=""
+})
+
+
+function getTempInCelcius(tempValue){
+return ((tempValue.value - 32) * 5/9).toFixed(2)
+ }
+ 
+function appendValue(value){
+h1Value.setAttribute('id',"result")
+h1Value.innerHTML=`${value} ° celcius`
+div.append(h1Value)
+}
+
+
