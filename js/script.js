4a2f9a5a13ffadbef587f30da9266e0c788faf9d
Moodybleu <meganbenn27@icloud.com>
2022-08-11 18:20:59 -0700

    finished tuning radio button and input space

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 console.log('Hello, front end');
 
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+  }
