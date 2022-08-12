57b207c8b2fc849fd78c40514e5612f751bf17d2
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-08-12 12:59:15 -0400

    temp convert homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello, front end');
+function fahrenheitConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+  }
+
+  function celsiusConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML = (valNum*1.8) + 32;
+  }
