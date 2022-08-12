cda702300e24d2da7791e80b60efc0428bffeb1a
devin-lynch <devin.l.lynch@gmail.com>
2022-08-12 00:44:46 -0500

    Style buttons and remove unnec code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-console.log('Hello, front end');
-
 // FAHRENHEIT TO CELSIUS FUNCTION
 function fahrToCel(fahrenheit){
     let fTemp = fahrenheit
     const fToC = (fTemp - 32) * 5 / 9;
     return fToC
 }
-console.log(fahrToCel(32))
-
 
 // CELSIUS TO FAHRENHEIT FUNCTION
 function celToFahr(celsius){
@@ -15,10 +11,6 @@ function celToFahr(celsius){
     const cToF = cTemp * 9 / 5 + 32;
     return cToF
 }
-console.log(celToFahr(32))
-
-
-
 
 // SUBMIT BUTTON VARIABLE
 let submitButton = document.querySelector('#submit')
@@ -33,20 +25,27 @@ function submitValue(){
     if(value === "C"){
      const result = fahrToCel(inputValue.value)
         console.log(result)
-        endResult.innerText = result
+        endResult.innerText = `${result}° C`
     } else {
      const result = celToFahr(inputValue.value)
         console.log(result)
-        endResult.innerText = result
+        endResult.innerText = `${result}° F`
     }  
 }
 
+let endResult = document.getElementById('result')
 let clearButton = document.querySelector('#clear')
 clearButton.addEventListener('click', clearResult);
 
 function clearResult(){
     endResult.innerText = ""
+    
 }
 
-let endResult = document.getElementById('result')
+// let clearInput = document.querySelector('#tempImput')
+// clearInput.addEventListener('click', clearTempInput)
+
+// function clearTempInput(){
+//     clearTempInput.innerText = ""
+// }
 
