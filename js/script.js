44034e22c8155b356a66183396f3612a74279d46
syorn96 <syorn2014@gmail.com>
2022-08-11 22:09:09 -0700

    adds changes to index.html, style.css, and script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-let input = document.querySelector('#input')
+//function conversions
+function convertF2C (input) {
+    return (input - 32) *5/9
+}
 
-console.log(input)
+function convertC2F (input) {
+    return (input * 9/5 + 32)
+}
+
+function convertK2C (input) {
+    return(input - 273.15)
+}
+
+function convertC2K (input) {
+    return (input + 273.15)
+}
+
+
+
+
+let input = document.getElementById('input')
+let clearButton = document.getElementById('clear').addEventListener("click", function(){
+    clear(input)
+})
+let selection1 = document.getElementById('selection1')
+let selection2 = document.getElementById('selection2')
+let submitButton = document.getElementById('submit').addEventListener("click", function(){
+    if (selection1.value === 'celcius' && selection2.value === 'fahrenheit') {
+        answer = convertC2F(input);
+        console.log(answer)
+    }
+})
+
+
+function clear (input) {
+    let value = input.value
+    
+}
