9f772838bbc4a05fc4c576f87bb8fef49b1ec781
syorn96 <syorn2014@gmail.com>
2022-08-12 09:56:30 -0700

    adds changes to css, html, js, with gabe

--- js/script.js
+++ js/script.js
@@ -1,38 +1,99 @@
-//function conversions
-function convertF2C (input) {
-    return (input - 32) *5/9
-}
+//Variable declarations
+let input = document.querySelector('#input')
+let fToC = document.querySelector('#fToC')
+let cToF = document.querySelector('#cToF')
+let clear = document.querySelector('#clear')
+let result = document.querySelector('#result')
 
-function convertC2F (input) {
-    return (input * 9/5 + 32)
-}
+//add event listeners
 
-function convertK2C (input) {
-    return(input - 273.15)
-}
-
-function convertC2K (input) {
-    return (input + 273.15)
-}
-
-
-
-
-let input = document.getElementById('input')
-let clearButton = document.getElementById('clear').addEventListener("click", function(){
-    clear(input)
+fToC.addEventListener("click", function(e){
+    let preconversion = input.value;
+    let postconversion = (preconversion - 32) * (5/9);
+    result.innertext = postconversion
+    console.log(result.innertext)
 })
-let selection1 = document.getElementById('selection1')
-let selection2 = document.getElementById('selection2')
-let submitButton = document.getElementById('submit').addEventListener("click", function(){
-    if (selection1.value === 'celcius' && selection2.value === 'fahrenheit') {
-        answer = convertC2F(input);
-        console.log(answer)
-    }
+
+cToF.addEventListener("click", function(e){
+    let preconversion = input.value;
+    let postconversion = ((preconversion * 5/9) + 32);
+    result.innertext = postconversion
+})
+
+clear.addEventListener("click", function(e){
+    result.innertext = ''
+    input.value = ''
 })
 
 
-function clear (input) {
-    let value = input.value
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //function conversions
+// function convertF2C(initialTemp) {
+//     return (initialTemp - 32) *5/9
+// }
+
+// function convertC2F(initialTemp) {
+//     return (initialTemp * 9/5 + 32)
+// }
+
+// function convertK2C(input) {
+//     return (input - 273.15)
+// }
+
+// function convertC2K(input) {
+//     return (input + 273.15)
+// }
+
+
+
+
+
+
+//my work
+// let input = document.getElementById('input')
+// let clearButton = document.getElementById('clear').addEventListener("click", function(){
+//     clear(input)
+// })
+// let selection1 = document.getElementById('selection1')
+// let selection2 = document.getElementById('selection2')
+// let submitButton = document.getElementById('submit').addEventListener("click", function(){
+//     getInputValue();
+//     if (selection1.value === 'celcius' && selection2.value === 'fahrenheit') {
+//         answer = convertC2F();
+//         console.log(answer)
+//     }
+// })
+
+
+
+// function clear (input) {
+//     let value = input.value
+//     console.log(value) 
+// }
+// function getInputValue(input){
+//     let initalTemp = input.value
+//     console.log(initalTemp)
+// }
