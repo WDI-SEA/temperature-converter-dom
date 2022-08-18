7d6f287b35027226ea9f9cdf899e84994d7dfc3b
yasahaverland <yasahaverland@icloud.com>
2022-08-17 21:23:28 -0400

    My Homowork is DONE

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, front end');
+const input = document.querySelector(".input")
+const fToCel = document.querySelector("#cel")
+const cToFafs = document.querySelector("#fafs")
+const result = document.querySelector("#result")
+const resetButton = document.querySelector(".resetbuttonn")
+
+fToCel.addEventListener("click", function(e) {
+    
+    let preConversion = input.value
+    let postConversion = (preConversion - 32) * (5/9)
+    result.innerHTML = postConversion
+    
+});
+
+cToFafs.addEventListener("click", function(e) {
+    let preConversion = input.value
+    let postConversion = (preConversion * 1.8) + 32
+    result.innerHTML = postConversion
+});
+
+resetButton.addEventListener("click", function(e) {
+    result.innerHTML = ''
+    input.value = ''
+});
