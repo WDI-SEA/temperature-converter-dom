27ab6e6ed542e77812e27e4e141c8364c433e824
HAB29 <Hab-8@live.com>
2022-10-20 11:49:31 +0300

    update

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    //all the dom manipulation goes here
+    console.log('DOM HAS LOADED')
+})
+
+console.log('Hello,from color palette picker!')
