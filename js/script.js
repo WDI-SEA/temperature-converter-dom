0e786c5cb334691eec36dd58a6d662477cd7f990
tylersb <tylersb@gmail.com>
2022-11-17 22:44:33 -0800

    adds project deliverables and deletes .editorconfig file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log('Hello, front end');
+({
+	plugins: ["jsdom-quokka-plugin"],
+	jsdom: { file: "seir-1114/unit1/temperature-converter-dom/index.html" }, // Located in project root
+});
+
+const btn = document.querySelector("#submit");
+const clr = document.querySelector("#clear");
+const output = document.querySelector("#output");
+const radioButtons = document.querySelectorAll('input[name="units"]');
+btn.addEventListener("click", () => {
+	let selectedTemp;
+	for (const radioButton of radioButtons) {
+		if (radioButton.checked) {
+			selectedTemp = radioButton.value;
+			break;
+		}
+	}
+
+    let inputTemp = document.querySelector('#temperature').value
+    console.log(selectedTemp)
+
+	if (selectedTemp === "Farenheit") {
+		outputTemp = (parseFloat(inputTemp) - 32) / 1.8;
+        console.log(outputTemp, selectedTemp)
+        output.innerText = outputTemp + " °C"
+	} else if (selectedTemp === "Celsius") {
+        outputTemp = parseFloat(inputTemp) * 1.8 + 32;
+        console.log(outputTemp, selectedTemp)
+        output.innerText = outputTemp + " °F"
+	}
+
+
+});
+
+clr.addEventListener("click", () => {
+    document.querySelector('#temperature').value = ""
+    output.innerText = ""
+})
