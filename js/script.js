448a74abc313312dfeb591f04c4b3b2f34788831
aridpen <115594817+aridpen@users.noreply.github.com>
2022-11-18 12:24:44 -0600

    Temp Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, front end');
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('reset').onclick = clearForm;
+
+function tempConvert() {
+    let fahrenheit = document.getElementById('fahrenheit').value;
+    let celsius = document.getElementById('celsius').value;
+
+    if (isNaN(fahrenheit) || isNaN(celsius)) {
+        alert('Please enter a valid number');
+        return
+    } else if (fahrenheit === '') {
+        fahrenheit = (parseFloat(celsius) * 1.8) + 32;
+    } else if (celsius === '') {
+        celsius = (parseFloat(fahrenheit - 32) / 1.8);
+    }
+
+
+
+
+    document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
+    document.getElementById('celsius').value = parseFloat(celsius).toFixed(1);
+}
+function clearForm() {
+    document.getElementById('fahrenheit').value = '';
+    document.getElementById('celsius').value = '';
+
+}
