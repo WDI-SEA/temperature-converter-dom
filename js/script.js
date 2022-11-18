55e4cb617833fa4a50ce6e7980e70a237581a9de
WHOVION <eszrajs@gmail.com>
2022-11-18 11:57:59 -0800

    my temp calc deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+let return = document.querySelector('.display')
+//event for inputToC button
+//formula  from f to c (32°F − 32) × 5/9 
+inputToC.addEventListener('click', function(e) {
+return((userInput - 32)*5/9)
+}
+
+//event for inputToF button
+//formula from c to f (32°C × 9/5) + 32
+inputToF.addEventListener('click', function(e) {
+r   eturn(userInput*9)/5+32)
+})
