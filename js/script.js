10c74bbdadd01ec6204187841f8b417670d7506e
PvrkFvmily <wlsgnl9973@gmail.com>
2022-11-18 09:00:20 -0800

    Added my not so awesome temp converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
-console.log('Hello, front end');
+// DOM selector
+const btn = document.getElementById('submit');
+const clear = document.querySelectorAll('clear');
+const fromVal = document.querySelectorAll('input[name="fromSelect"]');
+let output = document.getElementById('output');
+// Functions
+function unitChecker() {
+    if (inputSelectedUnit === "Celsius") {
+        celsiusToFahKel;
+    } else if (inputSelectedUnit === "Fahrenheit") {
+        fahrenheitToCelKel;
+    } else if (inputSelectedUnit === "Kelvin") {
+        kelvinToCelFah
+    }
+}
+
+function fahrenheitToCelKel() {
+    if (condition) {
+        
+    } else if () {
+
+    }
+}
+
+function celsiusToFahKel() {
+    if (condition) {
+        
+    } else if () {
+        
+    }
+}
+
+function kelvinToCelFah() {
+    if (){
+
+    } else if () {
+
+    }
+}
+
+function clearit() {
+    console.log('cleared');
+}
+
+// Event listeners
+btn.addEventListener("click", submitit());
+
+function submitit() {
+    let inputSelectedUnit;
+    for (const fromVal of value) {
+        if (fromVal.checked) {
+            inputSelectedUnit = fromVal.value;
+            break;
+        }
+    }
+    output.innerText = value
+}
+
+function checkOutput() {
+    
+}
+
+clear.addEventListener("click", clearit());
