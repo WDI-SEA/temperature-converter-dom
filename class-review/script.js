5044c3ff0834e74871183ff5a2abd21c8a978cc6
jtw007 <joshwu095@gmail.com>
2022-11-18 10:06:24 -0800

    mvp done

---
+++ class-review/script.js
@@ -0,0 +1,35 @@
+let titleText = document.getElementById('title')
+let input = document.getElementById('input')
+let buttonContainer = document.getElementById('button-container')
+let ftoc = document.getElementById('ftoc')
+let ctof = document.getElementById('ctof')
+let submit = document.getElementById('submit')
+let clear = document.getElementById('clear')
+let tempArea = document.getElementById('temp-area')
+let tempText = document.getElementById('temp-text')
+
+
+//F to C conversion
+let ftocConversion = function() {
+    let preConValue = input.value
+    let postConValue =(preConValue - 32) * (5/9)
+    tempText.innerText = postConValue
+}
+
+ftoc.addEventListener('click', ftocConversion) 
+
+
+//C to F conversion
+let ctofConversion = function() {
+    let preConValue = input.value
+    let postConValue =(preConValue * (9/5)) + 32
+    tempText.innerText = postConValue
+}
+
+ctof.addEventListener('click', ctofConversion) 
+
+//clear button
+clear.addEventListener('click', function(e){
+    input.value = ''
+    tempText.innerText ='Temp goes here'
+})
