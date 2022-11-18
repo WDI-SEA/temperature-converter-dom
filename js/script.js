a5a4dcff2c5ab600e28b4b0851eebaf68cd183d9
YOUR-USERNAME <gtellez07@yahoo.com>
2022-11-18 12:21:32 -0600

    Gonzalo color change done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
-console.log('Hello, front end');
+let titleText = document.getElementById('title')
+let input = document.getElementById('input')
+let ftoc = document.getElementById('ftoc')
+let ctof = document.getElementById('ctof')
+let clear = document.getElementById('clear')
+let tempArea = document.getElementById('temp-area')
+let tempText = document.getElementById('temp-text')
+
+//F to C conversion
+
+let ftocConversion = function() {
+    let preConValue = input.value
+    let postConValue = (preConValue -32) * (5/9)
+    tempText.innerText = postConValue
+}
+
+ftoc.addEventListener("click", ftocConversion)
+
+let ctofConversion = function() {
+    let preConValue = input.value
+    let postConValue = (preConValue * 9/5) +32
+    tempText.innerText = postConValue
+}
+
+ctof.addEventListener("click", ctofConversion)
+
+clear.addEventListener("click", function(e) {
+    input.value = " "
+    tempText.innerText = "temp goes here"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
