5452916e9d7e3ce5362d44bc9af4f62966e6088c
Gaymerbot <alexanderchmiel2@gmail.com>
2022-11-18 11:57:15 -0800

    completed deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,62 @@
-console.log('Hello, front end');
+// const form = document.querySelector('#form');
+// const input = document.querySelector('#temperature')
+// const display = document.querySelector('.display');
+// const clear = document.querySelector('#clear')
+// const valueOfInput = document.querySelector('input[name="genderS"]:checked').
+
+
+// temperature.addEventListener('submit',function(e){
+//     console.log('form submitted')
+//     console.log(input.value)
+//     input.value
+
+//    farenheitToCelcius(input.value);
+
+//    const result = docuement.createElement('p')
+//    result.textContent = farenheightToCelcius(input.value);
+//    display.appendChild(result);
+//     e.preventDefault();
+// });
+
+// clear.addEventListener('click', function(e){
+//     location.reload();
+// }
+
+// function farenheitToCelcius(userinput){
+//     return(`${userinput} degrees F = ${(userinput - 32)*(5/9)C';
+// }
+
+// function farenheitToKelvin(userinput){
+//     return(userinput - 32)*(5/9) + 273.15
+// }
+// location.reload()
+
+let titleText = document.getElementById('title')
+let input = document.getElementById('input')
+let ftoc = document.getElementById('ftoc')
+let ctof = document.getElementById('ctof')
+let submit = document.getElementById('submit')
+let clear = document.getElementById('clear')
+let tempArea =document.getElementById('tempArea')
+let tempText = document.getElementById('tempText')
+let buttonContainer = document.getElementById('buttonContainer')
+
+let ftocConversion = funtion() {
+    let preConValue = input.value;
+    let postConValue = (preConValue -32) * (5/9);
+    tempText.innerText = postConValue
+}
+
+let ctofConversion = funtion() {
+    let preConValue = input.value;
+    let postConValue = (preConValue * (9/5)) + 32;
+    tempText.innerText = postConValue
+}
+
+ftoc.addEventListener('click')
+
+clear.addEventListener('click', fuction(e) {
+    input.value = ''
+    tempText.innerText = ''
+
+})
