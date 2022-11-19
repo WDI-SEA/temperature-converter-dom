3c651bb04b29cc44b2ee72e32597930b7abb0932
emilyjryan <emilyryanmac@gmail.com>
2022-11-19 09:30:38 -0800

    updated temp converter with working radio buttons

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ let calculate = document.getElementById('calc-btn')
 let clear = document.getElementById('clear-btn')
 let result = document.getElementById('result')
 
-// Conversion function
+let ftocRadio = document.getElementById("radio1")
+let ctofRadio = document.getElementById("radio2")
 
-let calculateFtocConversion = function () {
+// Conversion function with radio buttons:
+
+
+let calculateConversion = function () { if(ftocRadio.checked === true) {
+
     let inputValue = input.value
     let convertedFtoc = (inputValue - 32) * (5 / 9)
     result.innerText = (`${convertedFtoc} °C`)
@@ -21,14 +26,29 @@ let calculateFtocConversion = function () {
     result.style.backgroundColor = "crimson"
     result.innerText = (`${convertedFtoc} °C is super hot!`)
     }
-} 
-calculate.addEventListener('click', calculateFtocConversion)
+} else if (ctofRadio.checked === true) {
+    let inputValue = input.value
+    let convertedCtof = (inputValue * (9/5) + 32)
+    result.innerText = (`${convertedCtof} °F`)
+
+    //  Bonus: change background color of result
+    if (convertedCtof <= 32) {
+    result.style.backgroundColor = "cornflowerblue"
+    result.innerText = (`${convertedCtof} °F is freeeeezing`)
+    }
+    if (convertedCtof >= 90) {
+    result.style.backgroundColor = "crimson"
+    result.innerText = (`${convertedCtof} °F is super hot!`)
+}
+}
+}
+calculate.addEventListener('click', calculateConversion)
 
 // Clear the input box with the clear button:
 clear.addEventListener('click', function(e) {
     input.value = '';
     result.innerText = "Result";
-    result.backgroundColor = null
+    result.style.backgroundColor = "rgb(33, 214, 81)";
 })
 
 
