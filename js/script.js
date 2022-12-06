9a8b68e90f99b4f4ce757d3e93f4399a4ea4c346
alidix <aliyahdixon4@gmail.com>
2022-12-06 11:54:49 -0500

    completed deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log('Hello, front end');
+
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
