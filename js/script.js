c2ad4dc5b4e1ef2642b9c11c185604fcc77f6bbe
Andrew <andrew@andrewsmith.org>
2023-03-09 18:56:58 -0500

    Add event listeners for buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello, front end');
+let input = document.querySelectorAll("input");
+let submitButton = document.querySelector("#submit-button");
+let clearButton = document.querySelector("#clear-button");
+
+const sayHello = () => {
+  console.log("hello");
+};
+
+const sayGoodbye = () => {
+  console.log("goodbye");
+};
+
+const addListeners = () => {
+  submitButton.addEventListener("click", sayHello);
+  clearButton.addEventListener("click", sayGoodbye);
+};
+
+addListeners();
