9c4cba3bd5abd5fa89d2e9f5d574e9514969fe25
Andrew <andrew@andrewsmith.org>
2023-03-09 22:17:38 -0500

    Wire up display button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let inputField = document.querySelector("input");
 let submitButton = document.querySelector("#submit-button");
 let clearButton = document.querySelector("#clear-button");
+let display = document.querySelector("#display");
 
 const sayHello = () => {
   console.log("hello");
@@ -10,12 +11,23 @@ const sayGoodbye = () => {
   console.log("goodbye");
 };
 
+const displayInput = () => {
+  input = inputField.value;
+  if (isNaN(input)) {
+    alert(`Ah dang.  ${input} isn't a number :(
+Please try again with a numeric value!`);
+  } else {
+    display.innerHTML = input;
+  }
+};
+
 const clearInput = () => {
   inputField.value = "";
+  display.innerHTML = "";
 };
 
 const addListeners = () => {
-  submitButton.addEventListener("click", sayHello);
+  submitButton.addEventListener("click", displayInput);
   clearButton.addEventListener("click", clearInput);
 };
 
