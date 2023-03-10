9899fb01b1347b25145bfd9058af70a2c8043511
al-eo <alicecamaro@gmail.com>
2023-03-10 08:56:32 -0800

    T convert HW

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log('Hello, front end');
+
+ let fToC = function(tempInF){
+    const tempInC = (tempInF - 32) * 5/9
+    return tempInC 
+ }
+// let cToF = function(tempInC){
+//     const tempInF = (tempInC * 9/5) + 32
+//     return tempInF
+// }
+ //the math
+//F->C (32°F − 32) × 5/9 = 0°C
+
+//(32°C × 9/5) + 32 = 89.6°F
+
+// temp input
+const input = document.querySelector("#Temperature")
+console.log(input)
+
+const result = document.querySelector("#result")
+console.log(`The temp is ${result}`)
+
+//action for submitting info
+const submitButton = document.querySelector("#submitButton");
+console.log(submitButton)
+submitButton.addEventListener('click', function(){
+    console.log(input.value)
+    const temp = fToC(input.value)
+    result.innerText = temp
+})
+
+// action to reset 
+const clearButton = document.querySelector("#clearButton");
+console.log(clearButton)
+clearButton.addEventListener("click", function(){
+    console.log("")
+      result.innerText = ""
+})
+
+/*set to clear answer and not input value, oops. At least something gets cleared?*/
