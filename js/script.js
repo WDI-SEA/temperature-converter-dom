cd68b2dad8106053cf5176d1457b714f7582ed89
brogers-97 <brburr24@gmail.com>
2023-03-10 09:32:29 -0700

    Fahrenheit converter Deliverable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('Hello, front end');
+
+
+const returnButton = document.querySelector("#returnTemp")
+const clearButton = document.querySelector("#getClear")
+const userInput = document.querySelector("#userInput")
+const answer = document.querySelector("#answer")
+
+
+returnButton.addEventListener("click", function() {
+    const inputValue = userInput.value;
+    const tempAnswer = (Math.floor((inputValue - 32)* 5/9))
+    console.log(inputValue)
+    answer.innerHTML = "Celcius is " + tempAnswer
+})
+
+clearButton.addEventListener("click", function() {
+    userInput.value = ""
+    answer.innerHTML = "Celsius is "
+})
