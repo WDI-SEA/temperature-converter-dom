0567097c64ca95d99a09b86c3e24af1e4f3b033b
awellsbiz <awellsbiz@gmail.com>
2023-03-10 08:50:47 -0800

    Temperature Convert 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,76 @@
-console.log('Hello, front end');
+// const celciusInput = document.getElementById("celcius");
+// const fahrenheitInput = document.getElementById("fahrenheit");
+
+// const inputs = document.getElementsByClassName("input");
+
+// for (let i = 0; i < inputs.length; i++) {
+//     let input = inputs[i];
+//     input.addEventListener("input", function (event){
+//         let value = event.target.value;
+//         console.log(event.target.name + ":" + value)
+//     })
+// }
+
+
+// Here I am capturing the two inputs.  to do that I have created variables
+
+// const body = document.querySelector("body")
+
+// const celciusInput = document.getElementById("celcius");
+// const fahrenheitInput = document.getElementById("fahrenheit");
+
+// const submitButton = document.querySelector("#submitButton")
+// const clearButton = document.querySelector("#clearButton")
+// console.log(clearButton)
+
+// clearButton.addEventListener("click", function() {
+//     const celciusInput = document.getElementById("celciusInput")
+//     const fahrenheitInput = document.getElementById("fahrenheitInput")
+//     console.dir(celciusInput)
+    
+// });
+// submitButton.addEventListener("click", function() {
+//     const celciusInput = document.getElementById("celciusInput")
+//     const fahrenheitInput = document.getElementById("fahrenheitInput")
+    // celciusInput.value = ""
+    // fahrenheitInput.value = ""
+//     console.log(celciusInput)
+    
+// });
+
+// here i am looping through all inputs (HTML collection)- creating a var to access all inputs
+// const inputs = document.getElementsByClassName("input")
+
+// console.log(inputs)
+
+// for(let i=0; i<inputs.length; i++) {
+//     let input = inputs[i];
+
+//     // input.addEventListener("input", function(event) {
+//     //     let value = parseFloat(event.target.value);
+//      } 
+
+// input.addEventListener(".submitButton", function(event) {
+//     event.preventDefault();
+//     let value = parseFloat(event.target.value);
+
+//     switch(event.target.name) {
+//         case "celcius":
+//             fahrenheitInput.value = (value * 1.8) + 32;
+//             break;
+//         case "fahrenheit":
+//             celciusInput.value = (value - 32) / 1.8;
+//             break;
+
+//     }
+// });
+// }
+
+// })
+// console.log(button)
+
+// *****************
+
+function convert() {
+    let temperature = 
+}
